ef5ce423120107896ef0a9892c9fc3b967c309ef
John Claro <jkrclaro@gmail.com>
2017-09-23 20:39:45 +0100

    Add django-registration

--- megaphone/urls.py
+++ megaphone/urls.py
@@ -17,10 +17,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
-from . import views as megaphone_views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='homepage.html'), name="homepage"),
+
+    url(r'^accounts/', include('accounts.urls')),
 
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
