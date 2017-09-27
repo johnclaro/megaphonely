75bb4f3edafee2ab00405b7f9a36ec0aaccae7a3
John Claro <jkrclaro@gmail.com>
2017-09-27 06:28:09 +0100

    Use now and publish date cannot be blank

--- contents/models.py
+++ contents/models.py
@@ -8,7 +8,7 @@ from django.utils.timezone import now
 
 class Content(models.Model):
     text = models.CharField(max_length=63206)
-    publish_date = models.DateTimeField(default=now(), blank=True)
+    publish_date = models.DateTimeField(default=now)
     twitter = models.BooleanField()
     facebook = models.BooleanField()
     instagram = models.BooleanField()
