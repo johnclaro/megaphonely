794815b4e3ab0323eba825fd9fb5f450fee32d4b
John Claro <jkrclaro@gmail.com>
2017-10-05 18:34:24 +0100

    Move inside megaphone

--- application.py
+++ application.py
@@ -1,15 +1,6 @@
-from flask import Flask
+from megaphone.app import create_app
 
-def create_app():
-    application = Flask(__name__)
-
-    from views.admin import admin
-    from views.home import home
-    application.register_blueprint(admin)
-    application.register_blueprint(home)
-
-    return application
+application = create_app()
 
 if __name__ == '__main__':
-    application = create_app()
     application.run()
