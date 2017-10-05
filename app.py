2f231ebb3d3a53642d36845e6379108195e26626
John Claro <jkrclaro@gmail.com>
2017-10-05 18:23:47 +0100

    Rename to application

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    application = Flask(__name__)
 
     from views.admin import admin
     from views.home import home
-    app.register_blueprint(admin)
-    app.register_blueprint(home)
+    application.register_blueprint(admin)
+    application.register_blueprint(home)
 
-    return app
+    return application
 
 def main():
     application = create_app()
