ce688aed2722b0c33deee2e10d19851864aa7bf9
John Claro <jkrclaro@gmail.com>
2017-10-05 19:41:42 +0100

    Load .env settings

--- application.py
+++ application.py
@@ -1,6 +1,14 @@
+import os
+from distutils.util import strtobool
+
+from dotenv import load_dotenv, find_dotenv
+
 from megaphone.app import create_app
 
+load_dotenv(find_dotenv())
 application = create_app()
 
 if __name__ == '__main__':
-    application.run()
+    application.run(
+        debug=bool(strtobool(os.environ['DEBUG']))
+    )
