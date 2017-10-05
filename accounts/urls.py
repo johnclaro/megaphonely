c1b0de34b1881416ddc93e4fb6d883199c510cfb
John Claro <jkrclaro@gmail.com>
2017-10-05 18:02:59 +0100

    delete django stuff

--- accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, include
-from . import views as account_views
-
-urlpatterns = [
-    url(r'', include('registration.backends.hmac.urls')),
-    url(r'profile/$', account_views.user_profile, name='user_profile'),
-]
--- contents/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from .views import ContentView
-
-urlpatterns = [
-    url(r'schedule/$', ContentView.as_view(), name='schedule_content'),
-]
