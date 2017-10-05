dbda3744a7df8609d7f7ea60b1ef50859f12c543
John Claro <jkrclaro@gmail.com>
2017-10-05 22:18:49 +0100

    Move db outside app and into models.py

--- megaphone/app.py
+++ megaphone/app.py
@@ -2,18 +2,13 @@ import os
 from distutils.util import strtobool
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = bool(strtobool(os.environ['SQLALCHEMY_TRACK_MODIFICATIONS']))
-    app.config['DEBUG'] = bool(strtobool(os.environ['DEBUG']))
-    app.config['PORT'] = int(os.environ['PORT'])
-    app.config['HOST'] = os.environ['HOST']
 
+    from .models import db
     db.init_app(app)
 
     from views.admin import admin
@@ -22,3 +17,11 @@ def create_app():
     app.register_blueprint(home)
 
     return app
+
+if __name__ == '__main__':
+    application = create_app()
+    application.run(
+        debug=bool(strtobool(os.environ['DEBUG'])),
+        port=int(os.environ['PORT']),
+        host=os.environ['HOST']
+    )
