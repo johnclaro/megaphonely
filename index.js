6e7f217f46a677a1bb6c158b29a045192b89044c
John Claro <jkrclaro@gmail.com>
2017-10-06 18:24:34 +0100

    Use sequelize to create user model

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const db = require('./models/')
+
+/**
+* Controllers
+**/
 const homeController = require('./controllers/home')
 
 app.get('/', homeController.index)
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+db
+  .sequelize
+  .sync({ force: true })
+  .then(function(err, msg) {
+    app.listen(process.env.PORT, function () {
+      console.log(`Example server listening on port ${process.env.PORT}!`)
+    })
+  })
