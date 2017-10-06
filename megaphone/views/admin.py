2b0c14c0f711013ab76f7edfb31a035cae38ff3c
John Claro <jkrclaro@gmail.com>
2017-10-06 17:52:52 +0100

    Use node

--- megaphone/views/admin.py
+++
@@ -1,6 +0,0 @@
-from flask import current_app, Blueprint, render_template
-admin = Blueprint('admin', __name__, url_prefix='/admin')
-
-@admin.route('/')
-def index():
-    return render_template('admin.html')
