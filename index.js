7a76b81db944634f3ea901500a984e3511505946
John Claro <jkrclaro@gmail.com>
2017-10-06 18:00:41 +0100

    Add homeController

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+const homeController = require('./controllers/home')
+
+app.get('/', homeController.index)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
