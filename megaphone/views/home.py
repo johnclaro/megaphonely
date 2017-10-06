a92ffb1b74ff858a787b1f752117c443adc36ae4
John Claro <jkrclaro@gmail.com>
2017-10-06 17:52:52 +0100

    Use node

--- megaphone/views/home.py
+++
@@ -1,6 +0,0 @@
-from flask import current_app, Blueprint, render_template
-home = Blueprint('home', __name__, url_prefix='')
-
-@home.route('/')
-def index():
-    return render_template('home.html')
