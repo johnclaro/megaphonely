713d1f67034af752259dac0a3e0da573a44e5f29
John Claro <jkrclaro@gmail.com>
2017-10-07 22:58:45 +0100

    Rename to app.js, download supertest

--- test/unit/controllers/content.test.js
+++ test/unit/controllers/content.test.js
@@ -1,8 +1,12 @@
 require('dotenv').config({path:'.env.test'})
+const request = require('supertest')
+const app = require('app.js')
 const expect = require('chai').expect
 
-describe('controllers/content', () => {
-  it('should get something', () => {
-    expect('moo').equal('moos')
+describe('GET /content/add', () => {
+  it('should return 200 OK', () => {
+    request(app)
+      .get('/')
+      .expect(200, done)
   })
 })
