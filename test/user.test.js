4e7203da84758f9ee8022cbfaaaf579b52a834f1
John Claro <jkrclaro@gmail.com>
2017-10-07 19:56:16 +0100

    Working tests

--- test/user.test.js
+++ test/user.test.js
@@ -1,30 +1,22 @@
 require('dotenv').config({path:'.env.test'})
 const expect = require('chai').expect
-const db = require('../models/')
+const User = require('../models/').User
 
 describe('users', () => {
 
-  after(function() {
-    db.sequelize.authenticate()
-      .then(function(msg) {
-        db.sequelize.close()
-      })
-  })
-
-  beforeEach(function() {
-    db.User.sync()
+  before(function() {
+    User.sync()
   })
 
   afterEach(function() {
-    db.User.destroy({truncate: true})
+    User.destroy({truncate: true})
   })
 
   it("should create user", function() {
-    const newUser = {
+    return User.create({
       email: 'a@gmail.com',
       password: '1234567'
-    }
-    return db.User.create(newUser)
+    })
       .then(function(user) {
         expect(user).to.be.a('object')
       })
