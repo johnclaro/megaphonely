8e5cda147f4e65913cf361d47fa7ff0d094ac4e8
John Claro <jkrclaro@gmail.com>
2017-10-08 01:16:28 +0100

    Upload models again

---
+++ models/content.js
@@ -0,0 +1,30 @@
+'use strict'
+
+module.exports = (db, Sequelize) => {
+  var Content = db.define('Content', {
+    message: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {msg: 'Message is required'},
+      }
+    },
+    publishAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      validate: {
+        isDate: true,
+        isAfterNow(value) {
+          if (value >= new Date()) {
+            throw new Error('Publish timestamp must be equal or greater than current time')
+          }
+        }
+      }
+    }
+  }, {
+    classMethods: {
+      associate: (models, cb) => {}
+    }
+  })
+  return Content
+}
