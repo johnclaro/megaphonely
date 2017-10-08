afdbb7f78888ce421ab1a6a88d6e807ce161e1c3
John Claro <jkrclaro@gmail.com>
2017-10-08 09:30:26 +0100

    Add test/helper.js

---
+++ test/helper.js
@@ -0,0 +1,6 @@
+/**
+* test/helper.js should always be ran first when mocha --recursive is executed.
+* This allows us to load the config file without having to specify through each
+* test file.
+**/
+require('dotenv').config({path:'.env.test'})
