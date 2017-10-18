4f60185a0afb07c02780abc443d07c8179b09df9
John Claro <jkrclaro@gmail.com>
2017-10-18 16:22:19 +0100

    Move passport to middlewares

--- controllers/account.js
+++ controllers/account.js
@@ -1,27 +1,4 @@
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
-
 const Account = require('models').Account
-
-passport.serializeUser((account, done) => {
-  done(null, account.id)
-})
-
-passport.deserializeUser((id, done) => {
-  Account.findById(id).then((account) => {
-    done(null, account)
-  }).catch((err) => {
-    done(err)
-  })
-})
-
-passport.use(new LocalStrategy({usernameField: 'email'}, (email, password, done) => {
-  Account.findAccount(email, password).then((account) => {
-    return done(null, account)
-  }).catch((err) => {
-    return done(err, null)
-  })
-}))
 
 exports.getAccount = (req, res, next) => {
   res.send('I am the account profile')
@@ -69,9 +46,4 @@ exports.getResetPassword = (req, res, next) => {
 
 exports.postResetPassword = (req, res, next) => {
   res.send({'Post reset password token:': req.query.token})
-}
-
-exports.isAuthenticated = (req, res, next) => {
-  if(req.isAuthenticated()) return next()
-  res.redirect('/login')
 }
