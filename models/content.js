0e2873c4531b6f75d9711b4015fb24b7f35b158c
John Claro <jkrclaro@gmail.com>
2017-10-24 22:12:10 +0100

    Add functionality to post content

--- models/content.js
+++ models/content.js
@@ -1,5 +1,27 @@
 'use strict'
 
+const schedule = require('node-schedule')
+const parser = require('cron-parser')
+const Twit = require('twit')
+
+function postTwitter(content, message){
+  var T = new Twit({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  })
+
+  T.post('statuses/update', {status: message}, (err, tweet, msg) => {
+    if (err) {
+      console.error(err.message)
+      return (err, null)
+    }
+    console.log('Successfully posted to twitter')
+    content.update({isTwitterPublished: true})
+  })
+}
+
 module.exports = (db, Sequelize) => {
   var Content = db.define('Content', {
     message: {
@@ -12,15 +34,14 @@ module.exports = (db, Sequelize) => {
     publishAt: {
       field: 'publish_at',
       type: Sequelize.DATE,
-      allowNull: false,
       validate: {
-        isDate: true,
-        isAfterNow(value) {
-          if (value >= new Date()) {
-            throw new Error('Publish timestamp must be equal or greater than current time')
-          }
-        }
+        isDate: true
       }
+    },
+    isTwitterPublished: {
+      field: 'is_twitter_published',
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
     },
     createdAt: {
       field: 'created_at',
@@ -35,5 +56,23 @@ module.exports = (db, Sequelize) => {
   })
 
   Content.associate = (models, cb) => {}
+  Content.schedule = (message, publishAt) => {
+    return Content.create({
+      message: message,
+      publishAt: publishAt
+    })
+    .then(content => {
+      var now = new Date();
+      now.setMinutes(now.getMinutes() + 1);
+      console.log(`Scheduled: ${now}`)
+      schedule.scheduleJob(now, (err, info) => {
+        postTwitter(content, message)
+      })
+      return (null, `Scheduled to post: ${message}`)
+    })
+    .catch(err => {
+      return (err, null)
+    })
+  }
   return Content
 }
