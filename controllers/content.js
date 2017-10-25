427c790613e098d34e1680e18fc05e96d8acc372
John Claro <jkrclaro@gmail.com>
2017-10-25 21:55:26 +0100

    Twitter account error handlers

--- controllers/content.js
+++ controllers/content.js
@@ -2,10 +2,17 @@ const Content = require('models').Content
 const TwitterAccount = require('models').TwitterAccount
 
 exports.postContent = (req, res, next) => {
+  if(!req.body.twitterIds) {
+    req.flash('error', 'You must choose a social media account to be posted')
+    return res.redirect('/dashboard')
+  }
   for(var i=0; i<req.body.twitterIds.length; i++) {
     TwitterAccount.findOne({where: {twitterId: req.body.twitterIds[i]}})
     .then(twitterAccount => {
-      if(!twitterAccount) return next(new Error(404))
+      if(!twitterAccount) {
+        req.flash('error', 'No twitter account found')
+        res.redirect('/dashboard')
+      }
       Content.schedule(
         req.body.message,
         req.body.publishAt,
