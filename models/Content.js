af4bb28ca3d06c423366ff63bd29b0a059994d42
John Claro <jkrclaro@gmail.com>
2017-10-25 18:47:37 +0100

    Rename model files with capitalization

---
+++ models/Content.js
@@ -0,0 +1,83 @@
+'use strict'
+
+const schedule = require('node-schedule')
+const Twit = require('twit')
+
+function postTwitter(message, accessTokenKey, accessTokenSecret) {
+  var T = new Twit({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: accessTokenKey,
+    access_token_secret: accessTokenSecret
+  })
+
+  return T.post('statuses/update', {status: message}, (err, tweet, msg) => {
+    if (err) {
+      console.error(`Error tweet: ${err}`)
+      return (err, null)
+    }
+    console.log(`Tweeted: ${message}`)
+  })
+}
+
+module.exports = (db, Sequelize) => {
+  var Content = db.define('Content', {
+    message: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {msg: 'Message is required'},
+      }
+    },
+    publishAt: {
+      field: 'publish_at',
+      type: Sequelize.DATE,
+      set: function(publishAt) {
+        if (publishAt) {
+          var publishAt = new Date(publishAt)
+        } else {
+          var publishAt = new Date()
+          publishAt.setSeconds(publishAt.getSeconds() + 15);
+        }
+        this.setDataValue('publishAt', publishAt)
+      }
+    },
+    isTwitterPublished: {
+      field: 'is_twitter_published',
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    },
+    createdAt: {
+      field: 'created_at',
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      field: 'updated_at',
+      type: Sequelize.DATE
+    }
+  }, {
+    tableName: 'contents'
+  })
+
+  Content.associate = (models, cb) => {}
+  Content.schedule = (message, publishAt, accessTokenKey, accessTokenSecret) => {
+    console.log(`Message: ${message} | Publish At: ${publishAt} | Access Token Key: ${accessTokenKey} | Access Token Secret: ${accessTokenSecret}`)
+    return Content.create({
+      message: message,
+      publishAt: publishAt
+    })
+    .then(content => {
+      schedule.scheduleJob(content.publishAt, (err, info) => {
+        postTwitter(message, accessTokenKey, accessTokenSecret)
+        .then(success => {
+          content.update({isTwitterPublished: true})
+        })
+      })
+      return (null, `Scheduled to post: ${message}`)
+    })
+    .catch(err => {
+      return (err, null)
+    })
+  }
+  return Content
+}
