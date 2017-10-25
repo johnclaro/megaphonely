58bf306a3982e94b6e2c24d20b097ea9bf6486c0
John Claro <jkrclaro@gmail.com>
2017-10-25 22:15:29 +0100

    Move res.redirect outside for loop

--- controllers/content.js
+++ controllers/content.js
@@ -5,32 +5,34 @@ exports.postContent = (req, res, next) => {
   if (!req.body.twitterIds) {
     req.flash('error', 'You must choose a twitter account')
     return res.redirect('/dashboard')
-  } else if (req.body.twitterIds instanceof Array){
-    var twitterIds = req.body.twitterIds
   } else {
-    var twitterIds = [req.body.twitterIds]
-  }
+    if (req.body.twitterIds instanceof Array){
+      var twitterIds = req.body.twitterIds
+    } else {
+      var twitterIds = [req.body.twitterIds]
+    }
 
-  for(var i=0; i<twitterIds.length; i++) {
-    console.log(twitterIds[i])
-    TwitterAccount.findOne({where: {twitterId: twitterIds[i]}})
-    .then(twitterAccount => {
-      if(!twitterAccount) {
-        req.flash('error', 'No twitter account found')
-        return res.redirect('/dashboard')
-      } else {
-        Content.schedule(
-          req.body.message,
-          req.body.publishAt,
-          twitterAccount.accessTokenKey,
-          twitterAccount.accessTokenSecret
-        )
-        req.flash('success', 'Succesfully scheduled twitter contents')
-        return res.redirect('/dashboard')
-      }
-    })
-    .catch(err => {
-      console.error(`Oh no: ${err}`)
-    })
+    for(var i=0; i<twitterIds.length; i++) {
+      TwitterAccount.findOne({where: {twitterId: twitterIds[i]}})
+      .then(twitterAccount => {
+        if(!twitterAccount) {
+          req.flash('error', 'No twitter account found')
+          return res.redirect('/dashboard')
+        } else {
+          Content.schedule(
+            req.body.message,
+            req.body.publishAt,
+            twitterAccount.accessTokenKey,
+            twitterAccount.accessTokenSecret
+          )
+        }
+      })
+      .catch(err => {
+        return next(err)
+      })
+    }
+
+    req.flash('success', 'Succesfully scheduled twitter contents')
+    return res.redirect('/dashboard')
   }
 }
