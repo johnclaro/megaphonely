d92d5e116260ecf29c9bbcfa4f7b7b29cd1ceee5
John Claro <jkrclaro@gmail.com>
2017-10-26 22:11:09 +0100

    Tests for POST /content

--- test/unit/content.test.js
+++ test/unit/content.test.js
@@ -25,4 +25,34 @@ describe('contents', () => {
     })
   })
 
+  describe('controllers', () => {
+    it('POST /content valid content', () => {
+      app.request.isAuthenticated = () => {return true}
+
+      var publishAt = new Date()
+      publishAt.setSeconds(publishAt.getSeconds() + 1);
+
+      return request(app)
+        .post('/content')
+        .send({message: '123', publishAt: publishAt})
+        .expect('Location', '/dashboard?content=Success')
+    })
+
+    it('POST /content invalid message', () => {
+      app.request.isAuthenticated = () => {return true}
+      return request(app)
+        .post('/content')
+        .send({message: ''})
+        .expect('Location', '/dashboard?content=Message%20cannot%20be%20empty')
+    })
+
+    it('POST /content invalid publishAt', () => {
+      app.request.isAuthenticated = () => {return true}
+      return request(app)
+        .post('/content')
+        .send({message: 'test', publishAt: '2016-10-10T12:12'})
+        .expect('Location', '/dashboard?content=Cannot%20schedule%20in%20the%20past')
+    })
+  })
+
 })
