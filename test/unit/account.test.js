4b86e0d5bb2b0731c98727f9af24a7afaea295e9
John Claro <jkrclaro@gmail.com>
2017-10-27 14:09:34 +0100

    Proper tests for account

--- test/unit/account.test.js
+++ test/unit/account.test.js
@@ -4,14 +4,21 @@ const request = require('supertest')
 const app = require('app.js')
 const Account = require('models').Account
 
+const agent = request.agent(app)
+
 describe('accounts', () => {
 
   before(() => {
-    return Account.sync({force: true}).then((msg) => {
-      Account.create({firstName: 'Jon', lastName: 'Snow', email: 'jonsnow@gmail.com', password: '1kn0wn0th1ng'})
-      Account.create({firstName: 'Tyrion', lastName: 'Lannister', email: 'tyrionlannister@gmail.com', password: 'tr14lbyf1r3'})
-      Account.create({firstName: 'Rob', lastName: 'Stark', email: 'robstark@gmail.com', password: 'r0bst4rk', passwordToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoicm9ic3RhcmtAZ21haWwuY29tIiwiaWF0IjoxNTA4MzUyNzIzfQ.70qzzfFCIhbfAt8Gy4t9kOQCngbolnXEzFUIvdNiLPg'})
-      Account.create({firstName: 'Tywin', lastName: 'Lannister', email: 'tywinlannister@gmail.com', password: 'tyw1nl4nn15t3r', passwordToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoidHl3aW5sYW5uaXN0ZXJAZ21haWwuY29tIiwiaWF0IjoxNTA4NDIxOTYzfQ.4abFuti_qwiXAG5CdmCbMURE3Pg9_MnhAHEt_OjpHzA'})
+    return Account.sync({force: true})
+    .then(account => {
+      // verificationToken
+      Account.create({firstName: 'valid', lastName: 'verification token', email: 'validverificationtoken@gmail.com'})
+
+      // passwordToken
+      Account.create({firstName: 'valid', lastName: 'password token', email: 'validpasswordtoken@gmail.com', password: 'r0bst4rk', passwordToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoicm9ic3RhcmtAZ21haWwuY29tIiwiaWF0IjoxNTA4MzUyNzIzfQ.70qzzfFCIhbfAt8Gy4t9kOQCngbolnXEzFUIvdNiLPg'})
+
+      // register
+      Account.create({firstName: 'foo', lastName: 'bar', email: 'foobar@gmail.com', password: 'foobar'})
     })
   })
 
@@ -19,112 +26,130 @@ describe('accounts', () => {
     return Account.destroy({truncate: true})
   })
 
-  describe('models', () => {
-    it('should create an account', () => {
-      const newAccount = {
-        firstName: 'Little',
-        lastName: 'Finger',
-        email: 'LITTLEFINGER@gmail.com',
-        password: 'ch405154l4dd3r'
-      }
-      return Account.create(newAccount)
-      .then((account) => {
-        expect(account.email).equal(newAccount.email.toLowerCase())
-        expect(account.passwordHash).not.equal(newAccount.password)
-      })
-    })
-
-    it('should get an account by ID', () => {
-      return Account.findById(1)
-      .then((account) => {
-        expect(account.email).equal('jonsnow@gmail.com')
-      })
-    })
-
-    it('should get an account by email and password', () => {
-      return Account.findAccount('jonsnow@gmail.com', '1kn0wn0th1ng')
-      .then((account) => {
-        expect(account.email).equal('jonsnow@gmail.com')
-      })
-    })
-
-    it('should generate a password token', () => {
-      return Account.emailPasswordToken('jonsnow@gmail.com')
-      .then((token) => {
-        expect(token).to.be.a('string')
-      })
-    })
-
-    it('should decrypt a password token', () => {
-      // This token does not have an expiry date !
-      // This token should give back a value of
-      //     { data: 'jonsnow@gmail.com', iat: 1507757856 }
-      // once it is decrypted properly
-      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiam9uc25vd0BnbWFpbC5jb20iLCJpYXQiOjE1MDc3NTc4NTZ9.Fe_0-GE76jiDz1-atXBnq6qhkziRVUjChppTPvK8ZVw'
-      return Account.verifyToken(token)
-      .then((decrypted) => {
-        expect(decrypted.data).equal('jonsnow@gmail.com')
-      })
-    })
-
-    it('should contain verification token for all accounts', () => {
-      Account.findAll()
-      .then(accounts => {
-        for (var i=0; i<accounts.length; i++) {
-          expect(accounts[i].verificationToken).to.be.a('string')
-        }
-      })
-    })
-  })
-
   describe('controllers', () => {
-    it('GET /login', () => {
-      return request(app)
-        .get('/settings')
-        .expect(302)
-        .expect('Location', '/login')
-    })
 
-    it('POST /login', () => {
-      return request(app)
-        .post('/login')
-        .send({email: 'valarmorghulis@gmail.com', password: 'br4v0s'})
-        .expect(302)
-        .expect('Location', '/login')
-    })
-
-    it('POST /register', () => {
-      return request(app)
-        .post('/register')
-        .send({
-          firstName: 'Khal',
-          lastName: 'Drogo',
-          email: 'khaldrogo@gmail.com',
-          password: 'd0thr4k1'
+    describe('verificationToken', () => {
+      it('GET /verifyverificationtoken valid verificationToken', () => {
+        return Account.findOne({
+          where: {email: 'validverificationtoken@gmail.com'}
         })
-        .expect(302)
-    })
-
-    it('POST /resetpassword valid token', () => {
-      return request(app)
-        .post('/resetpassword')
-        .send({
-          passwordToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoidHl3aW5sYW5uaXN0ZXJAZ21haWwuY29tIiwiaWF0IjoxNTA4NDIxOTYzfQ.4abFuti_qwiXAG5CdmCbMURE3Pg9_MnhAHEt_OjpHzA',
-          password: '1234567'
+        .then(account => {
+          return request(app)
+            .get(`/verifyverificationtoken/${account.verificationToken}`)
+            .expect('Location', '/dashboard?flash=Successfully%20verified%20account')
         })
-        .expect(302)
+      })
+
+      it('GET /verifyverificationtoken invalid verificationToken', () => {
+        return request(app)
+          .get('/verifyverificationtoken/1')
+          .expect(404)
+      })
     })
 
-    it('GET /verifypasswordtoken valid passwordToken', () => {
-      return request(app)
-        .get('/verifypasswordtoken/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoicm9ic3RhcmtAZ21haWwuY29tIiwiaWF0IjoxNTA4MzUyNzIzfQ.70qzzfFCIhbfAt8Gy4t9kOQCngbolnXEzFUIvdNiLPg')
-        .expect(302)
+    describe('passwordToken', () => {
+      it('GET /verifypasswordtoken valid passwordToken', () => {
+        return request(app)
+          .get('/verifypasswordtoken/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoicm9ic3RhcmtAZ21haWwuY29tIiwiaWF0IjoxNTA4MzUyNzIzfQ.70qzzfFCIhbfAt8Gy4t9kOQCngbolnXEzFUIvdNiLPg')
+          .expect('Location', '/resetpassword/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoicm9ic3RhcmtAZ21haWwuY29tIiwiaWF0IjoxNTA4MzUyNzIzfQ.70qzzfFCIhbfAt8Gy4t9kOQCngbolnXEzFUIvdNiLPg')
+      })
+
+      it('GET /verifypasswordtoken invalid passwordToken', () => {
+        return request(app)
+          .get('/verifypasswordtoken/1')
+          .expect(404)
+      })
+
+      it('POST /resetpassword valid password token', () => {
+        return request(app)
+          .post('/resetpassword/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoicm9ic3RhcmtAZ21haWwuY29tIiwiaWF0IjoxNTA4MzUyNzIzfQ.70qzzfFCIhbfAt8Gy4t9kOQCngbolnXEzFUIvdNiLPg')
+          .send({password: 'newpassword'})
+          .expect('Location', '/dashboard?Successfully%20updated%20password')
+      })
+
+      it('POST /resetpassword password too short', () => {
+        return request(app)
+          .post('/resetpassword/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoidHl3aW5sYW5uaXN0ZXJAZ21haWwuY29tIiwiaWF0IjoxNTA4NDIxOTYzfQ.4abFuti_qwiXAG5CdmCbMURE3Pg9_MnhAHEt_OjpHzA')
+          .send({password: 'new'})
+          .expect('Location', '/resetpassword/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoidHl3aW5sYW5uaXN0ZXJAZ21haWwuY29tIiwiaWF0IjoxNTA4NDIxOTYzfQ.4abFuti_qwiXAG5CdmCbMURE3Pg9_MnhAHEt_OjpHzA?flash=Password%20must%20contain%20at%20least%206%20characters%20long')
+      })
     })
 
-    it('GET /verifypasswordtoken invalid passwordToken', () => {
-      return request(app)
-        .get('/verifypasswordtoken/1')
-        .expect(404)
+    describe('register', () => {
+      it('POST /register valid user', () => {
+        return request(app)
+          .post('/register')
+          .send({
+            firstName: 'john bar',
+            email: 'johndoe@gmail.com',
+            password: 'johndoe'
+          })
+          .expect('Location', '/dashboard?flash=Register%20successful')
+      })
+
+      it('POST /register invalid email', () => {
+        return request(app)
+          .post('/register')
+          .send({email: '123'})
+          .expect('Location', '/dashboard?flash=Email%20is%20not%20valid')
+      })
+
+      it('POST /register first name empty', () => {
+        return request(app)
+          .post('/register')
+          .send({email: 'foobar@gmail.com', password: 'foobar', firstName: ''})
+          .expect('Location', '/dashboard?flash=Please%20enter%20your%20first%20name')
+      })
+
+      it('POST /register first name too long', () => {
+        return request(app)
+          .post('/register')
+          .send({
+            email: 'riverrock@gmail.com',
+            password: 'riverrock',
+            firstName: Math.random().toString(5).substr(2, 101)
+          })
+          .expect('Location', '/dashboard?flash=First%20name%20must%20be%20fewer%20than%20100%20characters')
+      })
+
+      it('POST /register last name too long', () => {
+        return request(app)
+          .post('/register')
+          .send({
+            email: 'riverrock@gmail.com',
+            password: 'riverrock',
+            firstName: Math.random().toString(5).substr(2, 100),
+            lastName: Math.random().toString(5).substr(2, 101)
+          })
+          .expect('Location', '/dashboard?flash=Last%20name%20must%20be%20fewer%20than%20100%20characters')
+      })
+
+      it('POST /register password too short', () => {
+        return request(app)
+          .post('/register')
+          .send({email: 'foobar@gmail.com', firstName: 'foobar', password: '123'})
+          .expect('Location', '/dashboard?flash=Password%20must%20contain%20at%20least%206%20characters%20long')
+      })
+
+      it('POST /register email already taken', () => {
+        return request(app)
+          .post('/register')
+          .send({
+            email: 'foobar@gmail.com',
+            password: 'foobar',
+            firstName: 'foo',
+            lastName: 'bar'
+          })
+          .expect('Location', '/dashboard?flash=This%20email%20is%20already%20taken')
+      })
     })
+
+    // it('POST /login agent', () => {
+    //   return agent
+    //     .post('/login')
+    //     .send({email: 'foobar@gmail.com', password: 'foobar'})
+    //     .expect(302)
+    //     .expect('Location', '/dashboard')
+    // })
   })
 })
