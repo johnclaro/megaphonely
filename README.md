6b28bfbb05453e5860a89ebbf208c910b606a4ae
John Claro <jkrclaro@gmail.com>
2017-10-30 11:28:41 +0000

    Ability to upload twitter video

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Social media management tool
 # Version
 - node v6.11.4 LTS
 - postgresql 9.6.2
+
+# Twitter video format
+Twitter expects an MPEG-4 Part 14 video format
+https://twittercommunity.com/t/uploading-video-chunked-error-on-finalize-command/67035/22
--- models/Content.js
+++ models/Content.js
@@ -12,24 +12,56 @@ function postTwitter(message, accessTokenKey, accessTokenSecret, file) {
   })
 
   if (file) {
-    return T.post('media/upload', {media_data: file}, (err, data, response) => {
-      if(err) console.error(err)
-      var mediaIdStr = data.media_id_string
-      var altText = message
-      var metaParams = { media_id: mediaIdStr, alt_text: { text: altText } }
-      T.post('/media/metadata/create', metaParams, (err, data, res) => {
-        var params = { status: message, media_ids: [mediaIdStr] }
-        T.post('statuses/update', params, (err, tweet, msg) => {
-          if (err) {
-            console.error(`Error tweet: ${err}`)
-            return (err, null)
+    const uploadsPath = `${__dirname.replace('/controllers', '').replace('/models', '')}/${file.destination}${file.filename}`
+    console.log(`Uploads path: ${uploadsPath}`)
+
+    if (file.mimetype == 'video/mp4') {
+      console.log(1)
+      T.postMediaChunked({file_path: uploadsPath}, (err, data, res) => {
+        var mediaIdStr = data.media_id_string
+        var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+        console.log(3)
+
+        T.post('media/metadata/create', meta_params, function (err, data, response) {
+          console.error(`Error: ${err}`)
+          console.log(`Data: ${data}`)
+          console.log(`Response: ${response}`)
+          if (!err) {
+            // now we can reference the media and post a tweet (media will attach to the tweet)
+            var params = { status: 'loving life #nofilter', media_ids: [mediaIdStr] }
+            console.log(4)
+            T.post('statuses/update', params, function (err, data, response) {
+              console.log('Done')
+            })
           }
-          console.log(`Tweeted: ${message}`)
         })
       })
-    })
+    } else {
+      console.log(2)
+      fs.readFile(uploadsPath, (err, data) => {
+        T.post('media/upload', {media_data: file}, (err, data, response) => {
+          if(err) console.error(err)
+          var mediaIdStr = data.media_id_string
+          var altText = message
+          var metaParams = { media_id: mediaIdStr, alt_text: { text: altText } }
+          T.post('/media/metadata/create', metaParams, (err, data, res) => {
+            var params = { status: message, media_ids: [mediaIdStr] }
+            T.post('statuses/update', params, (err, tweet, msg) => {
+              if (err) {
+                console.error(`Error tweet: ${err}`)
+                return (err, null)
+              }
+              console.log(`Tweeted: ${message}`)
+            })
+          })
+        })
+      })
+
+    }
   } else {
-    return T.post('statuses/update', {status: message}, (err, tweet, msg) => {
+    console.log(3)
+    T.post('statuses/update', {status: message}, (err, tweet, msg) => {
       if (err) {
         console.error(`Error tweet: ${err}`)
         return (err, null)
@@ -80,9 +112,6 @@ module.exports = (db, Sequelize) => {
     .then(content => {
       schedule.scheduleJob(content.publishAt, (err, info) => {
         postTwitter(message, accessTokenKey, accessTokenSecret, file)
-        .then(success => {
-          content.update({isPublished: true})
-        })
       })
       return (null, `Scheduled to post: ${message}`)
     })
