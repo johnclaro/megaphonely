2955e3d67cdeffdc3bb44edc5a996a53f4d8a962
John Claro <jkrclaro@gmail.com>
2017-11-03 23:44:20 +0000

    Properly render socials and contents in dashboard

--- controllers/home.js
+++ controllers/home.js
@@ -2,8 +2,10 @@
 
 const moment = require('moment-shortformat')
 
+const Account = require('models').Account
 const Social = require('models').Social
 const Content = require('models').Content
+const Schedule = require('models').Schedule
 
 exports.index = (req, res, next) => {
   if(req.user) return res.redirect('/dashboard')
@@ -19,30 +21,32 @@ exports.getPrivacy = (req, res, next) => {
 }
 
 exports.getDashboard = (req, res, next) => {
+  // Content.findAll({
+  //   include: [{model: Social, include: [{model: Account, where: {id: req.user.id}}]}]
+  // })
+  // .then(contents => {
+  //   console.log(JSON.stringify(contents,null,4))
+  // })
 
-  Social.findAll(
-    {
-      where: {
-        accountId: req.user.id,
-        isConnected: true
-      },
+  Promise.all([
+    Social.findAll({where: {accountId: req.user.id, isConnected: true}}),
+    Content.findAll({
       include: [{
-        model: Content
-      }],
-      order: [
-        [Content, 'publish_at', 'ASC']
-      ]
-    }
-  )
+        model: Social,
+        where: {accountId: req.user.id},
+        include: [{model: Account}]
+      }]
+    })
+  ])
   .then(results => {
+    // console.log(JSON.stringify(socials, null, 4))
+
+    console.log(JSON.stringify(results[1], null, 4))
     return res.render('dashboard', {
       title: 'Dashboard',
       account: req.user,
-      socials: results,
-      contents: results[0].Contents
+      socials: results[0],
+      contents: results[1]
     })
-  })
-  .catch(err => {
-    return next(err)
   })
 }
