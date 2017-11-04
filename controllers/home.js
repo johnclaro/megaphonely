39e39c4cf2946bda85d7baa7c479dbc1111d093e
John Claro <jkrclaro@gmail.com>
2017-11-04 00:19:21 +0000

    Clean up

--- controllers/home.js
+++ controllers/home.js
@@ -5,7 +5,6 @@ const moment = require('moment-shortformat')
 const Account = require('models').Account
 const Social = require('models').Social
 const Content = require('models').Content
-const Schedule = require('models').Schedule
 
 exports.index = (req, res, next) => {
   if(req.user) return res.redirect('/dashboard')
@@ -21,13 +20,6 @@ exports.getPrivacy = (req, res, next) => {
 }
 
 exports.getDashboard = (req, res, next) => {
-  // Content.findAll({
-  //   include: [{model: Social, include: [{model: Account, where: {id: req.user.id}}]}]
-  // })
-  // .then(contents => {
-  //   console.log(JSON.stringify(contents,null,4))
-  // })
-
   Promise.all([
     Social.findAll({where: {accountId: req.user.id, isConnected: true}}),
     Content.findAll({
@@ -39,9 +31,6 @@ exports.getDashboard = (req, res, next) => {
     })
   ])
   .then(results => {
-    // console.log(JSON.stringify(socials, null, 4))
-
-    console.log(JSON.stringify(results[1], null, 4))
     return res.render('dashboard', {
       title: 'Dashboard',
       account: req.user,
