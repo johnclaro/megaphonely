815b3e2024c72c922f85ba5102d39a73f035fec7
John Claro <jkrclaro@gmail.com>
2017-11-09 21:39:34 +0000

    To service

--- services/twitter/worker.js
+++ services/twitter/worker.js
@@ -1,7 +1,7 @@
 const kue = require('kue')
 const queue = kue.createQueue()
 
-const service = require('twitter')
+const service = require('service')
 
 queue.process('twitter', (job, done) => {
   service.post(job.data)
