de35192f2df94547cec017f37f502c7e0e3fee90
John Claro <jkrclaro@gmail.com>
2017-11-25 19:00:59 +0000

    Delete

--- megaphone/app/app.js
+++ megaphone/app/app.js
@@ -12,7 +12,6 @@ const favicon = require('serve-favicon')
 const crypto = require('crypto')
 const multerS3 = require('multer-s3')
 const aws = require('aws-sdk')
-const handlebarsHelpers = require('libs/handlebars/helpers')
 
 const app = express()
 const s3 = new aws.S3()
@@ -37,7 +36,6 @@ const upload = multer({
 **/
 app.engine('handlebars', exphbs({
   defaultLayout: 'main',
-  helpers: handlebarsHelpers,
   partialsDir: path.join(__dirname, 'views/partials')
 }))
 app.set('view engine', 'handlebars')
