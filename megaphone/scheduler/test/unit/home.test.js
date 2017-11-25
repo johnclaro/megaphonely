37cb71b91259272203d404e9fe71e074c0f6247b
John Claro <jkrclaro@gmail.com>
2017-11-25 19:22:47 +0000

    Test for each service

---
+++ megaphone/scheduler/test/unit/home.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest')
+const expect = require('chai').expect
+
+describe('home', () => {
+  describe('controllers', () => {
+    it('should return 200 OK', (done) => {
+      done()
+    })
+  })
+})
