1cb64988c9a21f4506e0dd2130448987e8ce6ea3
John Claro <jkrclaro@gmail.com>
2017-11-26 13:02:42 +0000

    Intentionally fail

--- megaphone/scheduler/test/unit/home.test.js
+++ megaphone/scheduler/test/unit/home.test.js
@@ -4,6 +4,7 @@ const expect = require('chai').expect
 describe('home', () => {
   describe('controllers', () => {
     it('should return 200 OK', (done) => {
+      expect(200).to.equal(201)
       done()
     })
   })
