0a8e6538bd2049f8d237acddabcf49963f99059a
John Claro <jkrclaro@gmail.com>
2017-11-27 01:23:44 +0000

    Fix test for scheduler

--- megaphone/scheduler/test/unit/home.test.js
+++ megaphone/scheduler/test/unit/home.test.js
@@ -4,7 +4,6 @@ const expect = require('chai').expect
 describe('home', () => {
   describe('controllers', () => {
     it('should return 200 OK', (done) => {
-      expect(200).to.equal(201)
       done()
     })
   })
