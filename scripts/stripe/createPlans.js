01b3f1886df259f9044b528d48bbc88f896b1a6e
John Claro <jkrclaro@gmail.com>
2017-12-09 16:18:52 +0000

    Change to Standard

--- scripts/stripe/createPlans.js
+++ scripts/stripe/createPlans.js
@@ -2,23 +2,12 @@ require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 stripe.plans.create({
-  name: 'Basic Plan',
-  id: 'basic-monthly',
-  interval: 'month',
-  currency: 'eur',
-  amount: 1000
-}, (err, plan) => {
-  console.error('Basic Error:', err)
-  console.log('Basic Plan:', plan)
-})
-
-stripe.plans.create({
-  name: 'Standard Plan',
+  name: 'Standard',
   id: 'standard-monthly',
   interval: 'month',
   currency: 'eur',
-  amount: 2000
+  amount: 1500
 }, (err, plan) => {
-  console.error('Standard Error:', err)
-  console.log('Standard Plan:', plan)
+  console.error('Basic Error:', err)
+  console.log('Basic Plan:', plan)
 })
