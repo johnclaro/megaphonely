d5fab7be8f95a427ca6f2f5dee674427d79bdf7c
John Claro <jkrclaro@gmail.com>
2017-12-10 18:17:58 +0000

    Use fluent-mmpeg

--- megaphone/publisher/libs/s3.js
+++ megaphone/publisher/libs/s3.js
@@ -1,5 +1,8 @@
+'use strict'
+
 const fs = require('fs')
 
+const ffmpeg = require('fluent-ffmpeg')
 const request = require('request')
 const aws = require('aws-sdk')
 
@@ -12,6 +15,7 @@ exports.download = (bucket, key, provider, cb) => {
     if(err) {
       console.error(err)
     } else {
+      console.log(data.Body)
       file.write(data.Body, (fileCreateFailed, fileCreated) => {
         if(fileCreateFailed) {
           cb(fileCreateFailed, null)
