0dce730f3db448b2973442a451f241c76ab6f2e3
John Claro <jkrclaro@gmail.com>
2017-12-10 18:18:54 +0000

    Call it lib

--- megaphone/publisher/index.js
+++ megaphone/publisher/index.js
@@ -7,7 +7,7 @@ const fs = require('fs')
 const kue = require('kue')
 
 const Schedule = require('models').Schedule
-const service = require(`libs/${provider}`)
+const service = require(`lib/${provider}`)
 const s3 = require('libs/s3')
 
 const queue = kue.createQueue({redis: {host: process.env.REDIS_HOST}})
