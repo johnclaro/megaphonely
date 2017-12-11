bd724fff7b17ab19196fb907e5fa70177a027fb7
John Claro <jkrclaro@gmail.com>
2017-12-11 20:56:56 +0000

    Rename to content

--- megaphone/publisher/lib/content.js
+++ megaphone/publisher/lib/content.js
@@ -26,3 +26,18 @@ exports.download = (bucket, key, provider, cb) => {
     }
   })
 }
+
+exports.convert = (filePath) => {
+  console.log(convert)
+  ffmpeg(filePath)
+    .videoCodec('libx264')
+    .audioCodec('libmp3lame')
+    .size('320x240')
+    .on('error', function(err) {
+      console.log('An error occurred: ' + err.message);
+    })
+    .on('end', function() {
+      console.log('Processing finished !');
+    })
+    .save('/path/to/output.mp4');
+}
