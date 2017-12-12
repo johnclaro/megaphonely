12b8f45e1b323f49b00da930592f6376e1fbd641
John Claro <jkrclaro@gmail.com>
2017-12-12 21:09:58 +0000

    Expect successful conversions

--- megaphone/publisher/test/unit/video.test.js
+++ megaphone/publisher/test/unit/video.test.js
@@ -3,18 +3,26 @@
 const path = require('path')
 const fs = require('fs')
 
+const replaceExt = require('replace-ext')
 const ffmpeg = require('fluent-ffmpeg')
 const expect = require('chai').expect
 
-const filePath = path.join(__dirname, '..', 'mp4')
-
 describe('home', () => {
   after(() => {
-    fs.unlink(mp4)
+    const filesToDelete = ['3gp', 'flv', 'mkv', 'mp4', 'ogv', 'webm']
+
+    for (let i=0; i<filesToDelete.length; i++) {
+      const fileToDelete = `${filesToDelete[i]}.mp4`
+      try {
+        fs.unlink(`${path.join(__dirname, '..', fileToDelete)}`)
+      } catch(err) {
+        console.error('Do nothing')
+      }
+    }
   })
 
   it('should convert mp4', (done) => {
-    ffmpeg(file.path)
+    ffmpeg(path.join(__dirname, '..', 'video.mp4'))
       .videoCodec('libx264')
       .audioCodec('libmp3lame')
       .on('error', function(err) {
@@ -24,30 +32,76 @@ describe('home', () => {
       .on('end', function() {
         done()
       })
-      .save(mp4);
+      .save(path.join(__dirname, '..', 'mp4.mp4'));
   })
 
   it('should convert mkv', (done) => {
-    done()
+    ffmpeg(path.join(__dirname, '..', 'video.mkv'))
+      .videoCodec('libx264')
+      .audioCodec('libmp3lame')
+      .on('error', function(err) {
+        console.log('An error occurred: ' + err.message);
+        done(err, null)
+      })
+      .on('end', function() {
+        done()
+      })
+      .save(path.join(__dirname, '..', 'mkv.mp4'));
   })
 
   it('should convert 3gp', (done) => {
-    done()
+    ffmpeg(path.join(__dirname, '..', 'video.3gp'))
+      .videoCodec('libx264')
+      .audioCodec('libmp3lame')
+      .on('error', function(err) {
+        console.log('An error occurred: ' + err.message);
+        done(err, null)
+      })
+      .on('end', function() {
+        done()
+      })
+      .save(path.join(__dirname, '..', '3gp.mp4'));
   })
 
-  it('should return 200 OK', (done) => {
-    done()
+  it('should convert flv', (done) => {
+    ffmpeg(path.join(__dirname, '..', 'video.flv'))
+      .videoCodec('libx264')
+      .audioCodec('libmp3lame')
+      .on('error', function(err) {
+        console.log('An error occurred: ' + err.message);
+        done(err, null)
+      })
+      .on('end', function() {
+        done()
+      })
+      .save(path.join(__dirname, '..', 'flv.mp4'));
   })
 
-  it('should return 200 OK', (done) => {
-    done()
+  it('should convert ogv', (done) => {
+    ffmpeg(path.join(__dirname, '..', 'video.ogv'))
+      .videoCodec('libx264')
+      .audioCodec('libmp3lame')
+      .on('error', function(err) {
+        console.log('An error occurred: ' + err.message);
+        done(err, null)
+      })
+      .on('end', function() {
+        done()
+      })
+      .save(path.join(__dirname, '..', 'ogv.mp4'));
   })
 
-  it('should return 200 OK', (done) => {
-    done()
-  })
-
-  it('should return 200 OK', (done) => {
-    done()
+  it('should convert webm', (done) => {
+    ffmpeg(path.join(__dirname, '..', 'video.webm'))
+      .videoCodec('libx264')
+      .audioCodec('libmp3lame')
+      .on('error', function(err) {
+        console.log('An error occurred: ' + err.message);
+        done(err, null)
+      })
+      .on('end', function() {
+        done()
+      })
+      .save(path.join(__dirname, '..', 'webm.mp4'));
   })
 })
