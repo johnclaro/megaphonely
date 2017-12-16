c7dbabcfd3aac615baa2a19675a566be3652a964
John Claro <jkrclaro@gmail.com>
2017-12-16 16:31:08 +0000

    Add frontend

---
+++ megaphone/frontend/README.md
@@ -0,0 +1,15 @@
+# Frontend
+
+Frontend for Megaphone
+
+# Setup
+```
+npm i -g webpack webpack-dev-server
+npm i
+npm run build
+```
+
+# Usage
+```
+npm run dev
+```
---
+++ megaphone/frontend/src/app.js
@@ -0,0 +1,10 @@
+import './css/style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './components/Home.js';
+
+ReactDOM.render(
+  <Home /> ,
+  document.querySelector('#root')
+);
