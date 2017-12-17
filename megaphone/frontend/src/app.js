fb2c24351ae1aa58651dc0345f7244043f17d91e
John Claro <jkrclaro@gmail.com>
2017-12-17 21:11:29 +0000

    [migrate] Basic routing

--- megaphone/frontend/src/app.js
+++ megaphone/frontend/src/app.js
@@ -1,6 +1,29 @@
+'use strict';
+
 import './css/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import routes from './routes'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-ReactDOM.render(routes, document.querySelector('#root'));
+import Home from './components/Home'
+import About from './components/About'
+
+ReactDOM.render((
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+    </div>
+  </Router>),
+  document.querySelector('#root'));
