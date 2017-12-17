3b690743d0890b6f6c968b04e2dfdc43ee259c87
John Claro <jkrclaro@gmail.com>
2017-12-17 15:46:23 +0000

    [migrate] Use jest for tests

---
+++ megaphone/backend/__tests__/account.test.js
@@ -0,0 +1,27 @@
+const expect = require('chai').expect
+const request = require('supertest')
+
+const app = require('index')
+const Account = require('models').Account
+
+describe('accounts', () => {
+
+  beforeAll(() => {
+    return Account.sync({force: true})
+    .then(account => {
+      Account.create({firstName: 'valid', lastName: 'verification token', email: 'validverificationtoken@gmail.com'})
+    })
+  })
+
+  it('GET / all accounts', () => {
+    return Account.findOne({
+      where: {email: 'validverificationtoken@gmail.com'}
+    })
+    .then(account => {
+      return request(app)
+        .get('/')
+        .expect(200)
+    })
+  })
+
+})
