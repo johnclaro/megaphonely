a55b505145c6eb335e1e6c6824a1c91c608b30d5
John Claro <jkrclaro@gmail.com>
2017-12-17 00:06:37 +0000

    Use spaces

--- megaphone/frontend/webpack.config.js
+++ megaphone/frontend/webpack.config.js
@@ -2,49 +2,49 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const port = process.env.PORT || 3000;
 const outputPath = path.join(__dirname, 'dist')
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: __dirname,
-		filename: 'dist/bundle.js'
-	},
-	resolve: {
-		modules: ['node_modules', './src'],
-		extensions: ['.js', '.jsx'],
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					use: 'css-loader!sass-loader'
-				}),
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					use: 'css-loader'
-				}),
-			},
-			{
-				test: /\.(js|jsx)$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/
-			}
-		]
-	},
-	plugins: [
-		new ExtractTextPlugin('bundle.css'),
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.EnvironmentPlugin({ NODE_ENV: process.env.NODE_ENV }),
-	],
-	devtool: 'source-map',
-	devServer: {
-		port,
-		hot: true,
-		historyApiFallback: true
-	}
+    entry: './src/index.js',
+    output: {
+        path: __dirname,
+        filename: 'dist/bundle.js'
+    },
+    resolve: {
+        modules: ['node_modules', './src'],
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        loaders: [{
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader!sass-loader'
+                }),
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader'
+                }),
+            },
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('bundle.css'),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.EnvironmentPlugin({
+            NODE_ENV: 'development'
+        }),
+    ],
+    devtool: 'source-map',
+    devServer: {
+        process.env.PORT || 3000,
+        hot: true,
+        historyApiFallback: true
+    }
 }
