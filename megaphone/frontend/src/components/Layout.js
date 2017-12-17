706df6dc3bdcba016ccfbbc544a73387db09a09d
John Claro <jkrclaro@gmail.com>
2017-12-17 22:27:20 +0000

    [migrate] Use classes

---
+++ megaphone/frontend/src/components/Layout.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+
+class Layout extends Component {
+  render() {
+    return (
+        <div className='container'>
+          <h1>Layout</h1>
+        </div>
+    )
+  }
+}
+
+export default Layout;
