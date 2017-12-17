98ac33e2b817a7d6b0995cfb252d413a628a14ed
John Claro <jkrclaro@gmail.com>
2017-12-17 01:37:23 +0000

    Many webpack configs

---
+++ megaphone/frontend/webpack/base.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+console.log(path.resolve(process.cwd(), './src/index'))
+module.exports = {
+  entry: {
+    app: path.resolve(process.cwd(), './src/index'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.EnvironmentPlugin([
+      'NODE_ENV',
+    ]),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+};
---
+++ megaphone/frontend/webpack/dev.config.js
@@ -0,0 +1,24 @@
+const merge = require('webpack-merge');
+const baseConfig = require('./base.config.js');
+
+module.exports = merge(baseConfig, {
+  devtool: 'eval-source-map',
+
+  devServer: {
+    inline: true,
+    contentBase: 'src',
+    port: '3000',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?importLoaders=1',
+        ],
+      },
+    ],
+  },
+});
