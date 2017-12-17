67544d1a27ab9edbe8e022ed8c5485943812c933
John Claro <jkrclaro@gmail.com>
2017-12-17 01:48:01 +0000

    Rename to app

--- megaphone/frontend/webpack/base.js
+++ megaphone/frontend/webpack/base.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: path.resolve(process.cwd(), './src/index'),
+    app: path.resolve(process.cwd(), './src/app'),
   },
 
   module: {
