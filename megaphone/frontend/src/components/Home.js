c80de2459901fb7ab6f08adfc18264382b75b03b
John Claro <jkrclaro@gmail.com>
2017-12-18 19:04:23 +0000

    [migrate] Home page

--- megaphone/frontend/src/components/Home.js
+++ megaphone/frontend/src/components/Home.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Home = () => (
-  <h1>Home</h1>
+  <div className='col-md-12'>
+    <div className='row'>
+      <div className='col-md-6'>
+        <h1>Social media management tool for small businesses</h1>
+        <p>
+          Megaphone helps you publish and schedule contents for your
+          social media accounts
+        </p>
+      </div>
+    </div>
+  </div>
 )
 
 export default Home;
