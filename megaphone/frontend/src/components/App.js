d9fec5c202727b32237399ccf9ca34e6c00bc9d2
John Claro <jkrclaro@gmail.com>
2017-12-18 19:22:25 +0000

    [migrate] Add footer and header

--- megaphone/frontend/src/components/App.js
+++ megaphone/frontend/src/components/App.js
@@ -4,13 +4,19 @@ import { Route, Switch } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Home from './Home'
 import NotFound from './NotFound';
+import Footer from './Footer';
+import Header from './Header';
 
 const App = () => (
-  <Switch>
-    <Route exact path='/' component={Home}/>
-    <Route path='/dashboard' component={Dashboard}/>
-    <Route component={NotFound} />
-  </Switch>
+  <div>
+    <Header />
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/dashboard' component={Dashboard}/>
+      <Route component={NotFound} />
+    </Switch>
+    <Footer />
+  </div>
 )
 
 export default App;
