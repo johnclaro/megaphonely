facec05a8f58b54e6b515bc25fe00d39fdb477d7
John Claro <jkrclaro@gmail.com>
2017-12-18 19:12:21 +0000

    [migrate] Move router

--- megaphone/frontend/src/components/App.js
+++ megaphone/frontend/src/components/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Switch
- } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Dashboard from './Dashboard';
 import Home from './Home'
 import NotFound from './NotFound';
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route path='/dashboard' component={Dashboard}/>
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route exact path='/' component={Home}/>
+    <Route path='/dashboard' component={Dashboard}/>
+    <Route component={NotFound} />
+  </Switch>
 )
 
 export default App;
