b6557737bd2925dc0bf992ab7f6a0f424577940c
John Claro <jkrclaro@gmail.com>
2017-12-19 14:36:46 +0000

    [migrate] Add login and register modals

--- megaphone/frontend/src/components/Header.js
+++ megaphone/frontend/src/components/Header.js
@@ -4,10 +4,42 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink
+  NavLink,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
 } from 'reactstrap';
 
+const pointerCursorStyle = {
+  cursor: 'pointer'
+}
+
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loginModal: false,
+      registerModal: false
+    };
+
+    this.toggleLoginModal = this.toggleLoginModal.bind(this);
+    this.toggleRegisterModal = this.toggleRegisterModal.bind(this);
+  }
+
+  toggleLoginModal() {
+    this.setState({
+      loginModal: !this.state.loginModal
+    });
+  }
+
+  toggleRegisterModal() {
+    this.setState({
+      registerModal: !this.state.registerModal
+    });
+  }
+
   render() {
     return (
       <div>
@@ -15,10 +47,35 @@ export default class Header extends React.Component {
           <NavbarBrand href='/'>Megaphone</NavbarBrand>
           <Nav className='ml-auto' navbar>
             <NavItem>
-              <NavLink href='/login'>Login</NavLink>
+              <NavLink onClick={this.toggleLoginModal} style={pointerCursorStyle}>Login</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink onClick={this.toggleRegisterModal} style={pointerCursorStyle}>Register</NavLink>
             </NavItem>
           </Nav>
         </Navbar>
+        <div>
+          <Modal isOpen={this.state.loginModal} toggleLoginModal={this.toggleLoginModal} className={this.props.className}>
+            <ModalHeader toggleLoginModal={this.toggleLoginModal}>Login</ModalHeader>
+            <ModalBody>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </ModalBody>
+            <ModalFooter>
+              <Button color='primary' onClick={this.toggleLoginModal}>Do Something</Button>{' '}
+              <Button color='secondary' onClick={this.toggleLoginModal}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+          <Modal isOpen={this.state.registerModal} toggleRegisterModal={this.toggleRegisterModal} className={this.props.className}>
+            <ModalHeader toggleRegisterModal={this.toggleRegisterModal}>Register</ModalHeader>
+            <ModalBody>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </ModalBody>
+            <ModalFooter>
+              <Button color='primary' onClick={this.toggleRegisterModal}>Do Something</Button>{' '}
+              <Button color='secondary' onClick={this.toggleRegisterModal}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+        </div>
       </div>
     );
   };
