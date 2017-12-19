9595fc30473a5de71dd9442c2dd1f7d52a805de5
John Claro <jkrclaro@gmail.com>
2017-12-19 13:55:23 +0000

    [migrate] More stuff

--- megaphone/frontend/src/components/Header.js
+++ megaphone/frontend/src/components/Header.js
@@ -8,21 +8,6 @@ import {
 } from 'reactstrap';
 
 export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  };
-
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  };
-
   render() {
     return (
       <div>
