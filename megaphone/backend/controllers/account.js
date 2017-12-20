64c05ebddf178d3c1515b9ad23776396ad56f524
John Claro <jkrclaro@gmail.com>
2017-12-20 10:56:31 +0000

    [migrate] Return if valid credentials

--- megaphone/backend/controllers/account.js
+++ megaphone/backend/controllers/account.js
@@ -3,15 +3,19 @@ const jwt = require('jsonwebtoken')
 const Account = require('models').Account
 
 exports.login = (req, res, next) => {
-  const expiresIn = {expiresIn: '1h'}
-  // TODO: Change 1 to be the user id in the database
-  jwt.sign({}, process.env.SECRET, expiresIn, (err, token) => {
-    if (err) {
-      res.json({'error': err})
-    } else {
-      res.json({token: token})
-    }
-  })
+  if (req.body.email == 'jkrclaro@outlook.com' && req.body.password == 'postmalone') {
+    const data = {email: req.body.email}
+    const expiresIn = {expiresIn: '1h'}
+    jwt.sign(data, process.env.SECRET, expiresIn, (err, token) => {
+      if (err) {
+        res.status(400).json({error: err})
+      } else {
+        res.json({token: token})
+      }
+    })
+  } else {
+    res.status(404).json({error: 'Invalid email or password'})
+  }
 }
 
 exports.settings = (req, res, next) => {
