5812fcd4f3de2d86736a19c21c2e18d33a51c85a
John Claro <jkrclaro@gmail.com>
2017-12-20 21:28:50 +0000

    [migrate] Fix tests

--- megaphone/backend/models/Account.js
+++ megaphone/backend/models/Account.js
@@ -1,11 +1,5 @@
 'use strict'
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const nodemailer = require('nodemailer');
-
-const salt = bcrypt.genSaltSync(parseInt(process.env.DB_SALT));
-
 module.exports = (db, Sequelize) => {
   var Account = db.define('Account', {
     first_name: Sequelize.STRING,
