32cddc0d003514eaf24fe857e1435ed10346c84d
John Claro <jkrclaro@gmail.com>
2017-12-20 21:18:54 +0000

    [migrate] Remove modals..... again

--- megaphone/frontend/src/components/Header.js
+++ megaphone/frontend/src/components/Header.js
@@ -6,40 +6,24 @@ import {
   Nav,
   NavItem,
   NavLink,
-  Modal,
-  ModalHeader,
-  ModalBody
+  NavbarToggler,
+  Collapse
 } from 'reactstrap';
-
-import LoginForm from './LoginForm';
-import SignupForm from './SignupForm';
-
-const pointerCursorStyle = {cursor: 'pointer'};
 
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {loginModal: false, signupModal: false};
 
-    this.toggleLogin = this.toggleLogin.bind(this);
-    this.toggleSignup = this.toggleSignup.bind(this);
-    this.redirectToDashboard = this.redirectToDashboard.bind(this);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
   }
 
-  toggleLogin() {
-    if (this.props.location.pathname === '/') {
-      this.setState({loginModal: !this.state.loginModal});
-    }
-  }
-
-  toggleSignup() {
-    if (this.props.location.pathname === '/') {
-      this.setState({signupModal: !this.state.signupModal});
-    }
-  }
-
-  redirectToDashboard() {
-    this.props.history.push('/dashboard');
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
   }
 
   render() {
@@ -47,29 +31,18 @@ class Header extends Component {
       <div>
         <Navbar color='faded' light expand='md'>
           <NavbarBrand href='/'>Megaphone</NavbarBrand>
-          <Nav className='ml-auto' navbar>
-            <NavItem>
-              <NavLink onClick={this.toggleLogin} style={pointerCursorStyle}>Login</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink onClick={this.toggleSignup} style={pointerCursorStyle}>Sign Up</NavLink>
-            </NavItem>
-          </Nav>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav className='ml-auto' navbar>
+              <NavItem>
+                <NavLink href='/login'>Login</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='/signup'>Sign Up</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
         </Navbar>
-        <div>
-          <Modal isOpen={this.state.loginModal} toggle={this.toggleLogin} className={this.props.className}>
-            <ModalHeader toggle={this.toggleLogin}>Login</ModalHeader>
-            <ModalBody>
-              <LoginForm redirectToDashboard={this.redirectToDashboard} modal={this.toggleLogin}/>
-            </ModalBody>
-          </Modal>
-          <Modal isOpen={this.state.signupModal} toggle={this.toggleSignup} className={this.props.className}>
-            <ModalHeader toggle={this.toggleSignup}>Sign Up</ModalHeader>
-            <ModalBody>
-              <SignupForm redirectToDashboard={this.redirectToDashboard} modal={this.toggleSignup}/>
-            </ModalBody>
-          </Modal>
-        </div>
       </div>
     );
   };
--- megaphone/frontend/src/components/Home.js
+++ megaphone/frontend/src/components/Home.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
   render() {
-    console.log('Rendering home page...')
     return (
       <h1>Home</h1>
     );
