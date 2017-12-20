e5d6b0dd34291c2277eb844081ebcd3eeadd9c77
John Claro <jkrclaro@gmail.com>
2017-12-20 19:26:16 +0000

    [migrate] Ternary

--- megaphone/backend/controllers/account.js
+++ megaphone/backend/controllers/account.js
@@ -4,17 +4,13 @@ const rp = require('request-promise')
 const Account = require('models').Account
 
 exports.new = (req, res, next) => {
+  const url = `http://${req.headers.host}/login`
   const { firstName, email , password, lastName='' } = req.body
   const user = {
     first_name: firstName, last_name: lastName, email: email, password: password
   }
   Account.create(user)
-  .then(account => {
-    const options = {
-      uri: `http://${req.headers.host}/login`, body: user, json: true
-    }
-    return rp.post(options)
-  })
+  .then(account => rp.post({uri: url, body: user, json: true}))
   .then(data => res.json(data))
   .catch(error => res.status(500).json({message: error}))
 }
@@ -23,12 +19,12 @@ exports.login = (req, res, next) => {
   const { email, password } = req.body
   Account.findOne({where: {email: email, password: password}})
   .then(account => {
-    if(!account) return res.status(401).json({message: 'Invalid email or password'})
+    if(!account) return res.status(401).json({message: 'Invalid credentials'})
     const data = {email: email}
     const expiresIn = {expiresIn: '1h'}
     jwt.sign(data, process.env.SECRET, expiresIn, (error, token) => {
       if (error) return res.status(500).json({message: 'Could not sign data'})
-      res.status(200).json({token: token})
+      return res.status(200).json({token: token})
     })
   })
   .catch(error => res.status(500).json({message: error}))
