343c6506a8ef1ab608a665313994610b312c541d
John Claro <jkrclaro@gmail.com>
2017-12-20 13:05:43 +0000

    [migrate] Query settings

--- megaphone/frontend/src/components/Dashboard.js
+++ megaphone/frontend/src/components/Dashboard.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Container } from 'reactstrap';
 
-const Dashboard = () => (
-  <h1>Dashboard</h1>
-)
-
-export default Dashboard;
+export default class Dashboard extends React.Component {
+  render() {
+    fetch('http://localhost:3001/settings', {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('jwt')
+      }
+    })
+    .then(success => success.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+    return (
+      <Container>
+        <h1>Dashboard</h1>
+      </Container>
+    );
+  };
+};
