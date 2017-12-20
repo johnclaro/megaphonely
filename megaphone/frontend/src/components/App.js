2f92848cc7ea2952c3ca774066453b1d46c15d63
John Claro <jkrclaro@gmail.com>
2017-12-20 12:25:12 +0000

    [migrate] Remove modals

--- megaphone/frontend/src/components/App.js
+++ megaphone/frontend/src/components/App.js
@@ -6,12 +6,16 @@ import Home from './Home'
 import NotFound from './NotFound';
 import Footer from './Footer';
 import Header from './Header';
+import Login from './Login';
+import Signup from './Signup';
 
 const App = () => (
   <div>
     <Header />
     <Switch>
       <Route exact path='/' component={Home}/>
+      <Route path='/login' component={Login}/>
+      <Route path='/signup' component={Signup}/>
       <Route path='/dashboard' component={Dashboard}/>
       <Route component={NotFound} />
     </Switch>
