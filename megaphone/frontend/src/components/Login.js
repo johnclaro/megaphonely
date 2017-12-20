46baf051b654aa2f61bc1c1cb68e0dd3af2171bb
John Claro <jkrclaro@gmail.com>
2017-12-20 21:18:54 +0000

    [migrate] Remove modals..... again

--- megaphone/frontend/src/components/Login.js
+++ megaphone/frontend/src/components/Login.js
@@ -4,11 +4,20 @@ import { Container } from 'reactstrap';
 import LoginForm from './LoginForm';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.redirectToDashboard = this.redirectToDashboard.bind(this);
+  }
+
+  redirectToDashboard() {
+    this.props.history.push('/dashboard');
+  }
+
   render() {
     return (
       <Container>
         <h1>Login</h1>
-        <LoginForm/>
+        <LoginForm redirectToDashboard={this.redirectToDashboard}/>
       </Container>
     );
   };
