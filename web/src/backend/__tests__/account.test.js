de7801518be775b1191b1d6d0f3ba04c21afb836
John Claro <jkrclaro@gmail.com>
2017-12-21 17:25:15 +0000

    Use johndoe.email

--- web/src/backend/__tests__/account.test.js
+++ web/src/backend/__tests__/account.test.js
@@ -30,7 +30,7 @@ describe('accounts', () => {
   })
 
   it('POST /login email exists but invalid password', () => {
-    const invalid = {email: 'johndoe@gmail.com', password: '123456'}
+    const invalid = {email: johndoe.email, password: '123456'}
     return request(app).post('/account').send(johndoe).expect(200)
     .then(account => request(app).post('/login').send(invalid).expect(401))
     .then(response => expect(response.body.message).to.equal('Invalid credentials'))
