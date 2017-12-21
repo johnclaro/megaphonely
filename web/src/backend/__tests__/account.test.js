32a6d0bde2f98f47806cb6ea492631cfc6b6cb40
John Claro <jkrclaro@gmail.com>
2017-12-21 17:24:10 +0000

    Properly compare hashed password

--- web/src/backend/__tests__/account.test.js
+++ web/src/backend/__tests__/account.test.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const app = require('../app');
 const Account = require('../models').Account;
 
-const johndoe = {firstName: 'John', lastName: 'Doe', email: 'johndoe@outlook.com', password: 'johndoe'};
+const johndoe = {firstName: 'John', lastName: 'Doe', email: 'johndoe@gmail.com', password: 'johndoe'};
 const foobar = {firstName: 'Foo', email: 'foobar@gmail.com', password: 'foobar'};
 
 
@@ -13,22 +13,9 @@ describe('accounts', () => {
 
   beforeEach(() => Account.sync({force: true}))
 
-  it.only('POST /account create new account', () => {
+  it('POST /account create new account', () => {
     return request(app).post('/account').send(johndoe)
-    .then(response => {
-      const saltRounds = parseInt(process.env.SALT_ROUNDS)
-      let user = {
-        firstName: johndoe.firstName,
-        lastName: johndoe.lastName,
-        email: johndoe.email,
-        password: bcrypt.hashSync(johndoe.password, saltRounds).length,
-        id: 1,
-        created_at: response.body.created_at,
-        updated_at: response.body.updated_at
-      }
-      response.body.password = response.body.password.length || 0
-      expect(user).to.deep.equal(response.body)
-    })
+    .then(response => expect(johndoe).to.deep.equal(response.body))
   })
 
   it('POST /login valid credentials', () => {
@@ -36,8 +23,17 @@ describe('accounts', () => {
     .then(created => request(app).post('/login').send(johndoe).expect(200))
   })
 
-  it('POST /login invalid credentials', () => {
-    return request(app).post('/login').send(foobar).expect(401)
+  it('POST /login account does not exist', () => {
+    return request(app).post('/account').send(johndoe).expect(200)
+    .then(account => request(app).post('/login').send(foobar).expect(401))
+    .then(response => expect(response.body.message).to.equal('Invalid credentials'))
+  })
+
+  it('POST /login email exists but invalid password', () => {
+    const invalid = {email: 'johndoe@gmail.com', password: '123456'}
+    return request(app).post('/account').send(johndoe).expect(200)
+    .then(account => request(app).post('/login').send(invalid).expect(401))
+    .then(response => expect(response.body.message).to.equal('Invalid credentials'))
   })
 
   it('GET /settings with no token', () => {
