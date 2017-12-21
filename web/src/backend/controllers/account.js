a4372b61ace45adba2a9231a27beeb615ed02cd8
John Claro <jkrclaro@gmail.com>
2017-12-21 17:24:10 +0000

    Properly compare hashed password

--- web/src/backend/controllers/account.js
+++ web/src/backend/controllers/account.js
@@ -7,18 +7,18 @@ const { LoginValidator, SignupValidator } = require('../validators')
 exports.create = (req, res, next) => {
   const { firstName, email , password, lastName='' } = req.body;
   const saltRounds = parseInt(process.env.SALT_ROUNDS) || 12
-
-  SignupValidator.validate({ firstName, lastName, email, password})
-  .then(account => {
-    return bcrypt.hash(account.password, saltRounds)
+  const account = { firstName, lastName, email, password }
+  SignupValidator.validate(account)
+  .then(validated => {
+    return bcrypt.hash(validated.password, saltRounds)
     .then(hashed => {
-      account.password = hashed
-      return Promise.resolve(account)
+      validated.password = hashed
+      return Promise.resolve(validated)
     })
     .catch(error => Promise.reject(error))
   })
-  .then(valid => Account.create(valid))
-  .then(data => res.json(data))
+  .then(newAccount => Account.create(newAccount))
+  .then(created => res.json(account))
   .catch(error => {
     const message = error.errors[0].message || error.message
     return res.status(500).json({message: message})
@@ -28,14 +28,17 @@ exports.create = (req, res, next) => {
 exports.login = (req, res, next) => {
   const { email, password } = req.body
   LoginValidator.validate({ email, password })
-  .then(valid => Account.findOne({where: { email, password }}))
+  .then(valid => Account.findOne({where: { email }}))
   .then(account => {
     if(!account) return res.status(401).json({message: 'Invalid credentials'})
-    const data = {email: email}
-    const expiresIn = {expiresIn: '1h'}
-    jwt.sign(data, process.env.SECRET, expiresIn, (error, token) => {
-      if (error) return res.status(500).json({message: 'Could not sign data'})
-      return res.status(200).json({token: token})
+    return bcrypt.compare(password, account.password)
+    .then(matched => {
+      if (!matched) return res.status(401).json({message: 'Invalid credentials'})
+      const expiresIn = {expiresIn: '1h'}
+      jwt.sign({}, process.env.SECRET, expiresIn, (error, token) => {
+        if (error) return res.status(500).json({message: 'Could not sign data'})
+        return res.status(200).json({token: token})
+      })
     })
   })
   .catch(error => res.status(500).json({message: error}))
