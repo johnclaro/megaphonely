fed84d876a17193f9b863a149385f46245ab7985
John Claro <jkrclaro@gmail.com>
2017-12-22 22:09:44 +0000

    Properly use status codes and promisify

--- src/backend/controllers/account.js
+++ src/backend/controllers/account.js
@@ -2,57 +2,56 @@
 
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const promisify = require('bluebird').promisify;
 
 const Account = require('../models').Account;
-const { LoginValidator, SignupValidator } = require('../validators');
 const emailer = require('../lib/emailer');
-const saltRounds = parseInt(process.env.SALT_ROUNDS) || 12
+const { jwtSign } = require('../lib/promisify');
+const { LoginValidator, SignupValidator } = require('../validators');
+
+const { SECRET, SALT_ROUNDS } = process.env;
 
 exports.signup = (req, res, next) => {
   const { firstName, email , password, lastName='' } = req.body;
-  const account = { firstName, lastName, email, password }
+  const account = { firstName, lastName, email, password };
   SignupValidator.validate(account)
   .then(validated => {
-    return bcrypt.hash(validated.password, saltRounds)
+    return bcrypt.hash(validated.password, parseInt(SALT_ROUNDS))
     .then(hashed => {
-      validated.password = hashed
-      return Promise.resolve(validated)
-    })
-    .catch(error => Promise.reject(error))
+      validated.password = hashed;
+      return Promise.resolve(validated);
+    });
   })
   .then(newAccount => Account.create(newAccount))
   .then(created => res.json(account))
   .catch(error => {
-    const message = error.errors[0].message || error.message
-    return res.status(500).json({message: message})
+    // SequelizeUniqueConstraintError or ValidationError
+    const message = error.errors[0].message || error.errors[0];
+    message ? res.status(400).json({ message }) : next(error);
   });
 };
 
 exports.login = (req, res, next) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
   LoginValidator.validate({ email, password })
   .then(valid => Account.findOne({where: { email }}))
   .then(account => {
-    if(!account) return res.status(401).json({message: 'Invalid credentials'})
+    if(!account) return res.status(401).send()
     return bcrypt.compare(password, account.password)
     .then(matched => {
-      if (!matched) return res.status(401).json({message: 'Invalid credentials'})
-      return promisify(jwt.sign)({}, process.env.SECRET, {expiresIn: '1h'})
-      .then(token => res.status(200).json({token: token}))
-      .catch(error => res.status(500).json({message: 'Could not sign data'}))
+      if (!matched) return res.status(401).send()
+      return jwtSign({}, SECRET, {expiresIn: '1h'})
+      .then(token => res.json({ token }))
     })
-    .catch(error => Promise.reject(error))
   })
-  .catch(error => res.status(500).json({message: error}))
+  .catch(error => next(error))
 }
 
 exports.forgotPassword = (req, res, next) => {
   const email = req.body.email;
   Account.findOne({where: { email }})
   .then(account => {
-    if (!account) return Promise.resolve({message: 'Email sent'})
-    return promisify(jwt.sign)({ email }, process.env.SECRET, {expiresIn: '1h'})
+    if (!account) return res.status(200).send()
+    return jwtSign({ email }, SECRET, {expiresIn: '1h'})
     .then(token => {
       const subject = 'Reset your megaphone password';
       const html = `
@@ -80,10 +79,11 @@ exports.forgotPassword = (req, res, next) => {
       return emailer.send(email, subject, html)
     })
   })
-  .then(sent => res.json({message: 'Email sent'}))
-  .catch(error => res.status(500).json(error))
+  .then(sent => res.status(200).send())
+  .catch(error => next(error))
 }
 
 exports.settings = (req, res, next) => {
-  return res.json({message: 'settings!'})
+  const message = 'settings!'
+  return res.json({ message })
 }
--- src/backend/jest.config.js
+++ src/backend/jest.config.js
@@ -6,12 +6,11 @@ process.env.DB_HOST='host'
 process.env.DB_DIALECT='sqlite'
 process.env.DB_STORAGE='test.sqlite3'
 process.env.DB_LOGGING=true
-process.env.SALT_ROUNDS=10
+process.env.SALT_ROUNDS=1
 process.env.SECRET='secret'
 process.env.EMAIL_HOST='smtp.ethereal.email'
 process.env.EMAIL='rbxou4rilv2ifu7h@ethereal.email'
 process.env.EMAIL_PASSWORD='KGkwfamWQEqcu5yTp9'
-
 
 module.exports = {
   verbose: true,
