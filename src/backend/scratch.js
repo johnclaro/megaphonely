8ae6815c851d960340f3cfbeccdac4f52f7591ee
John Claro <jkrclaro@gmail.com>
2017-12-22 16:30:38 +0000

    Remove scratch.js

--- src/backend/scratch.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const jwt = require('jsonwebtoken');
-const promisify = require('bluebird').promisify;
-// exports.jwtSign = promisify(jwt.sign);
-
-promisify(jwt.sign)({}, '213')
-.then(success => console.log(success))
-.catch(error => console.error(error))
