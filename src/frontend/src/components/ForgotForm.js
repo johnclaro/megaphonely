c7635006725a8ef911a90736b1fe61d9e26b434a
John Claro <jkrclaro@gmail.com>
2017-12-23 20:44:23 +0000

    Add Forgot

---
+++ src/frontend/src/components/ForgotForm.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { Formik } from 'formik';
+import { Button, Form, Input, FormGroup } from 'reactstrap';
+
+import { ForgotValidator } from '../validators';
+import { forgot, alert } from '../apis';
+
+class ForgotForm extends React.Component {
+  render() {
+    const { openAlert } = this.props;
+    return (
+      <Formik
+        validationSchema={ForgotValidator}
+        initialValues={{
+          email: 'jkrclaro@outlook.com'
+        }}
+        onSubmit={(
+          values,
+          { setSubmitting, setErrors }
+        ) => {
+          forgot(values)
+          .then(response => alert(openAlert, 'Hello', 'success'))
+          .catch(error => alert(openAlert, error, 'danger'))
+        }}
+        render={({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <Form onSubmit={handleSubmit}>
+            <FormGroup>
+              <Input
+                type='email'
+                name='email'
+                placeholder='Email'
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              {touched.email && errors.email && <div className='error-input'>{errors.email}</div>}
+            </FormGroup>
+            <Button className='btn-block' type='submit' disabled={isSubmitting}>
+              Send me instructions
+            </Button>
+          </Form>
+        )}
+      />
+    )
+  }
+}
+
+export default withRouter(ForgotForm)
