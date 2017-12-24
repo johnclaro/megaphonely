fd250994178264a12df3228a7ad4d29f1d992ce4
John Claro <jkrclaro@gmail.com>
2017-12-24 02:07:39 +0000

    Reset form

--- src/frontend/src/apis/index.js
+++ src/frontend/src/apis/index.js
@@ -1,25 +1,33 @@
-function login(input) {
+function login(data) {
   return fetch('http://localhost:3001/login', {
     method: 'post',
     headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(input)
+    body: JSON.stringify(data)
   })
 }
 
-function signup(input) {
+function signup(data) {
   return fetch('http://localhost:3001/signup', {
     method: 'post',
     headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(input)
+    body: JSON.stringify(data)
   })
 }
 
-function forgot(input) {
+function forgot(data) {
   return fetch('http://localhost:3001/forgot', {
     method: 'post',
     headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(input)
+    body: JSON.stringify(data)
   })
 };
 
-module.exports = { signup, login, forgot };
+function reset(data, token) {
+  return fetch('http://localhost:3001/reset', {
+    method: 'post',
+    headers: {'Content-Type': 'application/json', 'Authorization': token},
+    body: JSON.stringify(data)
+  })
+};
+
+module.exports = { signup, login, forgot, reset };
