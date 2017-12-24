1495f1e02fc7eab4fc7ecd5fb8e244907a54f927
John Claro <jkrclaro@gmail.com>
2017-12-24 12:29:50 +0000

    Proper expriation

--- src/backend/middlewares/jwt.js
+++ src/backend/middlewares/jwt.js
@@ -4,12 +4,12 @@ module.exports = function(req, res, next) {
   if (req.hasOwnProperty('headers') && req.headers.hasOwnProperty('authorization')) {
     jwt.verify(req.headers['authorization'], process.env.SECRET, (err, data) => {
       if (err) {
-        return res.status(401).json()
+        return res.status(401).json({})
       } else {
         next(data)
       }
     })
   } else {
-    return res.status(401).json()
+    return res.status(401).json({})
   }
 }
