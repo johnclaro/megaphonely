2d1e10cbed5fb1ae04efdea4a9543c6aae572977
John Claro <jkrclaro@outlook.com>
2018-01-06 16:11:41 +0000

    Sample accounts

---
+++ src/accounts/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User, Group
+from rest_framework.viewsets import ModelViewSet
+from src.accounts.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
