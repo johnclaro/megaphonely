cda19b290682998faf9740a62ae942383bf5ba52
John Claro <jkrclaro@gmail.com>
2018-01-06 16:30:47 +0000

    Move src2

--- src2/backend2/README.md
+++
@@ -1,7 +0,0 @@
-# megaphone-backend
-
-Backend for Megaphone
-
-# Versions
-- Node.js v8.9.3
-- npm v5.5.1
--- src2/backend2/controllers/health.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-exports.index = (req, res, next) => {
-  return res.json({'health': 'alive'})
-}
