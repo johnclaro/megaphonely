1d479578568fe6b40b0c3ceb4a7e5f935f5617cf
John Claro <jkrclaro@gmail.com>
2018-01-07 16:46:41 +0000

    URLs for social auth

--- src/urls.py
+++ src/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
