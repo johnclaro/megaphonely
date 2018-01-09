b96e7d0d03fac8e4aed115fc5621e9871d34711f
John Claro <jkrclaro@outlook.com>
2018-01-09 18:24:20 +0000

    Remove apps

--- src/urls.py
+++ src/urls.py
@@ -3,18 +3,14 @@ from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from rest_framework.documentation import include_docs_urls
 
-from .profiles.views import UserViewSet, GroupViewSet
-
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^auth/', include('djoser.urls')),
-    url(r'^auth/', include('djoser.urls.jwt')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^oauth/', include('social_django.urls', namespace='social')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
     url(r'^docs/', include_docs_urls(title='Einstein')),
+    url(r'^accounts/', include('djoser.urls')),
+    url(r'^', include('djoser.urls.jwt')),
+    url(r'^drf/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
