814a594c2389b0a663a5e55cd711384047f5ef9d
John Claro <jkrclaro@outlook.com>
2018-01-17 18:18:28 +0000

    Explicit

--- einstein/urls.py
+++ einstein/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework import documentation
 from rest_framework.routers import DefaultRouter
-from rest_framework.documentation import include_docs_urls
 
 from contents.views import ContentViewSet
 
@@ -12,7 +12,7 @@ router.register(r'contents', ContentViewSet)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^docs/', include_docs_urls(title='Einstein')),
+    url(r'^docs/', documentation.include_docs_urls(title='Einstein')),
     url(r'^accounts/', include('djoser.urls')),
     url(r'^accounts/', include('djoser.urls.jwt')),
     url(r'^drf/', include('rest_framework.urls', namespace='rest_framework')),
