b949e50aeebd9b120fee1f5a595a003d5e7a78a0
John Claro <jkrclaro@gmail.com>
2018-01-19 18:38:21 +0000

    Contents should be related as a ManyToManyField with SocialAccount

--- contents/views.py
+++ contents/views.py
@@ -1,17 +1,23 @@
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from django.contrib.auth.models import User
+from allauth.socialaccount.models import SocialAccount
 
 from .models import Content
-from .serializers import ContentSerializer, UserSerializer
+from .serializers import (ContentSerializer, UserSerializer,
+                          SocialAccountSerializer)
 
 
 class ContentViewSet(ModelViewSet):
-    """API endpoint to get contents"""
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
 
-class UserViewSet(ReadOnlyModelViewSet):
-    """API endpoint to get users"""
+
+class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SocialAccountViewSet(ModelViewSet):
+    queryset = SocialAccount.objects.all()
+    serializer_class = SocialAccountSerializer
