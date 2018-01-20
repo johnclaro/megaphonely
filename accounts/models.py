18ccbe45329cd5cd0f99cbc4587f7ced931192b0
John Claro <jkrclaro@gmail.com>
2018-01-20 08:48:07 +0000

    Add Profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
+from allauth.socialaccount.models import SocialAccount
+
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    social = models.ManyToManyField(SocialAccount)
