ecb328ad85f4e6455d97377fd72e5132caf7ef8d
John Claro <jkrclaro@gmail.com>
2018-01-24 22:53:28 +0000

    Add Socials to Web browsable

--- contents/models.py
+++ contents/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+from accounts.models import Social
+
 
 class Content(models.Model):
     message = models.TextField()
+
     user = models.ForeignKey(
         'auth.User', related_name='contents', on_delete=models.CASCADE
     )
+    socials = models.ManyToManyField(Social)
--- einstein/urls.py
+++ einstein/urls.py
@@ -4,13 +4,14 @@ from django.contrib import admin
 from rest_framework import documentation
 from rest_framework.routers import DefaultRouter
 
-from accounts.views import UserViewSet, ProfileViewSet
+from accounts.views import UserViewSet, ProfileViewSet, SocialViewSet
 from contents.views import ContentViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'contents', ContentViewSet)
 router.register(r'profiles', ProfileViewSet)
+router.register(r'socials', SocialViewSet)
 
 admin.autodiscover()
 
