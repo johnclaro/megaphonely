fe7888b8a08c76a1c3caa7380e3d024723bcb93c
John Claro <jkrclaro@outlook.com>
2018-01-27 18:13:54 +0000

    Use a test twitter app

--- contents/tests.py
+++ contents/tests.py
@@ -18,8 +18,8 @@ class Tasks(TestCase):
         ['video', video]
     ])
     def test_publish_content_to_twitter(self, name, media):
-        access_token_key = '901476753272655872-K8eWjpaTwj0OyyQcPwz9PXdRv0DZVls'
-        access_token_secret = 'CcucxBFJq3fn8EX8brGNpwZ1e2uIuzxX5sWolGQRCywMJ'
+        access_token_key = '901476753272655872-JljfulA4wX6KBvLLCwtpcNdXPzZAXjQ'
+        access_token_secret = 'AYnPcwdpGiqdDiXJNmOHv9DbcEqLF3tPtK7nhgj7Iyjf2'
         message = datetime.datetime.now().strftime('%Y-%m-%d-%s')
         result = publish_content_to_twitter(access_token_key,
                                             access_token_secret,
