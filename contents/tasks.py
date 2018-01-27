585f427ae51d26ed93e859c1722f5ee9db6f8323
John Claro <jkrclaro@outlook.com>
2018-01-27 17:57:52 +0000

    Ability to publish tweet with image or video

--- contents/tasks.py
+++ contents/tasks.py
@@ -4,11 +4,12 @@ from celery import shared_task
 from django.conf import settings
 
 @shared_task
-def publish_content_to_twitter(access_token_key, access_token_secret, message):
+def publish_content_to_twitter(access_token_key, access_token_secret, message,
+                               media=None):
     api = twitter.Api(
         consumer_key=settings.SOCIAL_AUTH_TWITTER_KEY,
         consumer_secret=settings.SOCIAL_AUTH_TWITTER_SECRET,
         access_token_key=access_token_key,
         access_token_secret=access_token_secret,
     )
-    return api.PostUpdate(message)
+    return api.PostUpdate(message, media)
