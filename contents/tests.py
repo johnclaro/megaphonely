d4c41348250222c55517765db5acb89199a32aec
John Claro <jkrclaro@gmail.com>
2018-01-27 17:39:48 +0000

    Add celery task to publish twitter status

--- contents/tests.py
+++ contents/tests.py
@@ -1,10 +1,19 @@
-from contents.tasks import add
+import datetime
+
+from twitter.models import Status
 
 from django.test import TestCase
+
+from contents.tasks import publish_content_to_twitter
 
 
 class Tasks(TestCase):
 
-    def test_add(self):
-        result = add(1, 2)
-        self.assertEqual(result, add(1, 2))
+    def test_publish_content_to_twitter(self):
+        access_token_key = '901476753272655872-K8eWjpaTwj0OyyQcPwz9PXdRv0DZVls'
+        access_token_secret = 'CcucxBFJq3fn8EX8brGNpwZ1e2uIuzxX5sWolGQRCywMJ'
+        message = datetime.datetime.now().strftime('%Y-%m-%d-%s')
+        result = publish_content_to_twitter(access_token_key,
+                                            access_token_secret,
+                                            message)
+        self.assertEqual(Status, type(result))
