3f8d8cf2115eef66e5f36d51072fe7bcf1971e7b
John Claro <jkrclaro@gmail.com>
2018-01-28 15:42:36 +0000

    Add ability to post a text to facebook

--- contents/tasks.py
+++ contents/tasks.py
@@ -1,12 +1,21 @@
+import boto3
 import twitter
 from facepy import GraphAPI
 from celery import shared_task
 
 from django.conf import settings
 
+
+def get_s3_file(s3_filename):
+    bucket = settings.AWS_STORAGE_BUCKET_NAME
+    client = boto3.client('s3')
+    s3_object = client.get_object(Bucket=bucket, Key=s3_filename)
+    return s3_object['Body'].read()
+
+
 @shared_task
 def publish_to_twitter(access_token_key, access_token_secret, message,
-                               media=None):
+                       media=None):
     api = twitter.Api(
         consumer_key=settings.SOCIAL_AUTH_TWITTER_KEY,
         consumer_secret=settings.SOCIAL_AUTH_TWITTER_SECRET,
@@ -17,6 +26,8 @@ def publish_to_twitter(access_token_key, access_token_secret, message,
 
 
 @shared_task
-def publish_to_facebook(access_token_key, message, media=None):
+def publish_to_facebook(access_token_key, id, message, media=None):
     api = GraphAPI(access_token_key)
-    api.post()
+    path = 'me/feed'.format(id=id)
+    data = {'message': message}
+    return api.post(path=path, **data)
