81018c08386bb7c2417182c66ec709ad3980eeb4
John Claro <jkrclaro@gmail.com>
2018-01-31 21:32:33 +0000

    Upload content to facebook or twitter

--- contents/tasks.py
+++ contents/tasks.py
@@ -10,21 +10,44 @@ from django.conf import settings
 
 
 def get_s3_file_streaming_body(key: str) -> StreamingBody:
-    bucket = settings.AWS_STORAGE_BUCKET_NAME
-    streaming_body = boto3.resource('s3').Object(bucket, key).get()['Body']
+    s3_object = boto3.resource('s3').Object(settings.AWS_STORAGE_BUCKET_NAME,
+                                            key)
+    streaming_body = s3_object.get()['Body']
     return streaming_body
+
+
+def get_s3_file_presigned_url(key: str) -> str:
+    s3_client = boto3.client('s3')
+    params = {'Bucket': settings.AWS_STORAGE_BUCKET_NAME, 'Key': key}
+    presigned_url = s3_client.generate_presigned_url('get_object', params)
+    return presigned_url
+
+
+def download_s3_file(key: str) -> None:
+    s3_object = boto3.resource('s3').Object(settings.AWS_STORAGE_BUCKET_NAME,
+                                            key)
+    s3_object.download_file(key)
 
 
 @shared_task
 def publish_to_twitter(access_token_key, access_token_secret, message,
-                       media=None):
+                       image=None, video=None):
     api = twitter.Api(
         consumer_key=settings.SOCIAL_AUTH_TWITTER_KEY,
         consumer_secret=settings.SOCIAL_AUTH_TWITTER_SECRET,
         access_token_key=access_token_key,
         access_token_secret=access_token_secret,
     )
-    return api.PostUpdate(message, media)
+    data = {}
+    if video:
+        # TODO: Would prefer to just supply an S3 pre signed url
+        download_s3_file(video)
+        data['media'] = open(video, 'rb')
+        os.remove(video)
+    elif image:
+        data['media'] = get_s3_file_presigned_url(image)
+    response = api.PostUpdate(message, **data)
+    return response
 
 
 @shared_task
@@ -32,24 +55,16 @@ def publish_to_facebook(access_token_key, message, image=None, video=None):
     # https://developers.facebook.com/docs/graph-api/common-scenarios/
     data = {'message': message}
     api = GraphAPI(access_token_key)
+
     if video:
         api.url = 'https://graph-video.facebook.com'
         data['path'] = 'me/videos'
-
-        # TODO: Use get_s3_file_streaming_body instead
-        bucket = settings.AWS_STORAGE_BUCKET_NAME
-        boto3.resource('s3').Object(bucket, video).download_file(video)
-        with open(video, 'rb') as read_file:
-            data['source'] = read_file
-            response = api.post(**data)
-        os.remove(video)
-
+        data['file_url'] = get_s3_file_presigned_url(video)
     elif image:
         data['path'] = 'me/photos'
         data['source'] = get_s3_file_streaming_body(image)
-        response = api.post(**data)
     else:
         data['path'] = 'me/feed'
-        response = api.post(**data)
 
+    response = api.post(**data)
     return response
--- contents/tests.py
+++ contents/tests.py
@@ -7,32 +7,40 @@ from django.test import TestCase
 
 from contents import tasks
 
+MESSAGE = datetime.datetime.now().strftime('%Y-%m-%d-%s')
+IMAGE = 'small.jpg'
+VIDEO = 'small.mp4'
 
-class Tasks(TestCase):
-    MESSAGE = datetime.datetime.now().strftime('%Y-%m-%d-%s')
-    IMAGE_URL = 'http://www.catster.com/wp-content/uploads/2017/06/small-kitten-meowing.jpg'
-    VIDEO_URL = 'http://techslides.com/demos/sample-videos/small.mp4'
-    IMAGE_FILENAME = 'small-kitten-meowing.jpg'
-    VIDEO_FILENAME = 'small.mp4'
 
-    # @parameterized.expand([
-    #     ['text', None],
-    #     ['image', IMAGE_URL],
-    #     ['video', VIDEO_URL]
-    # ])
-    # def test_publish_to_twitter(self, name, media):
-    #     access_token_key = '901476753272655872-D2BwU3Z7vKJzv023g3gpBcdAfMBE1Ez'
-    #     access_token_secret = 'oRnzSQ1eHMQBKot6R6QZdApn3wk6ZdarPo8FaKK0bWyzN'
-    #     result = tasks.publish_to_twitter(access_token_key,
-    #                                       access_token_secret,
-    #                                       self.MESSAGE,
-    #                                       media=media)
-    #     self.assertEqual(Status, type(result))
+class TwitterTasks(TestCase):
 
     @parameterized.expand([
-        # ['text', None],
-        # ['image', IMAGE_URL],
-        ['video', VIDEO_FILENAME]
+        ['text', None],
+        ['image', IMAGE],
+        ['video', VIDEO]
+    ])
+    def test_publish_to_twitter(self, name, media):
+        data = {name: media}
+        try:
+            del data['text']
+        except KeyError:
+            pass
+
+        access_token_key = '901476753272655872-D2BwU3Z7vKJzv023g3gpBcdAfMBE1Ez'
+        access_token_secret = 'oRnzSQ1eHMQBKot6R6QZdApn3wk6ZdarPo8FaKK0bWyzN'
+        result = tasks.publish_to_twitter(access_token_key,
+                                          access_token_secret,
+                                          MESSAGE,
+                                          **data)
+        self.assertEqual(Status, type(result))
+
+
+class FacebookTasks(TestCase):
+
+    @parameterized.expand([
+        ['text', None],
+        ['image', IMAGE],
+        ['video', VIDEO]
     ])
     def test_publish_to_facebook(self, name, media):
         data = {name: media}
@@ -42,7 +50,5 @@ class Tasks(TestCase):
             pass
 
         access_token_key = 'EAAY8CZCqoStABAFN1RHBA0DBcBDX4NGXZB5l6EbjXGucQZC5ybSJkeRaGfiUuMcElrJj9Wx7e6MGNtPIZBwWZAtASPBZBYFHw2slxYApRl8zzyR57dQEDZBZCkc1ohUBDsTCwSgscJaXVNlxcsJzwQdLKYkbdTUS9kZBlHhq0cTpwXAZDZD'
-        result = tasks.publish_to_facebook(access_token_key,
-                                           self.MESSAGE,
-                                           **data)
+        result = tasks.publish_to_facebook(access_token_key, MESSAGE, **data)
         self.assertEqual(dict, type(result))
