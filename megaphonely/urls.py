da45079a7802c43d7460e5437e66783fe95517dd
John Claro <jkrclaro@outlook.com>
2018-02-01 18:37:19 +0000

    Use allauth

--- megaphonely/urls.py
+++ megaphonely/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^social/', include('social_django.urls', namespace='social'))
 ]
