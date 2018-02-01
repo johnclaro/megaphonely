b06919700b210d02b4c0c7fdad4bc34e5a52a68a
John Claro <jkrclaro@outlook.com>
2018-02-01 18:32:00 +0000

    Render home.html in templates

--- megaphonely/settings.py
+++ megaphonely/settings.py
@@ -97,7 +97,7 @@ MIDDLEWARE = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'megaphonely/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
