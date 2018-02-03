27f604848250f3e5141464167f56fc3c7c028c27
John Claro <jkrclaro@gmail.com>
2018-02-03 19:15:25 +0000

    More stuff

--- megaphonely/dashboard/models.py
+++ megaphonely/dashboard/models.py
@@ -5,6 +5,6 @@ from django.conf import settings
 class Content(models.Model):
     message = models.TextField()
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+    account = models.ForeignKey(settings.AUTH_USER_MODEL,
                              related_name='contents',
                              on_delete=models.CASCADE)
