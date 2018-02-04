48f6ba6bc7eadadfb36ae1be649eb3c2282b1e49
John Claro <jkrclaro@gmail.com>
2018-02-04 01:31:34 +0000

    Storage optimizations

--- src/storage.py
+++ src/storage.py
@@ -1,7 +1,11 @@
-from storages.backends.s3boto3 import S3Boto3Storage
-
 from django.conf import settings
 
+from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticStorage(S3Boto3Storage):
+
+class Static(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
+
+
+class Media(S3Boto3Storage):
+    location = settings.MEDIAFILES_LOCATION
