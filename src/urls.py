17d4733ee2a3ba398844afd3a405e23e85a93b6e
John Claro <jkrclaro@gmail.com>
2018-02-04 17:15:59 +0000

    Login and logout

--- src/urls.py
+++ src/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.contrib import admin
-from django.conf.urls import url, include
+from django.conf.urls import include
 from django.views.generic import TemplateView
 
 from src.contents.views import ContentCreate, ContentDelete, ContentUpdate
@@ -9,11 +9,11 @@ from src.contents.views import ContentCreate, ContentDelete, ContentUpdate
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^accounts/', include('allauth.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^social/', include('social_django.urls', namespace='social')),
+    re_path(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    re_path(r'^accounts/', include('allauth.urls')),
+    path(r'admin/', admin.site.urls),
+    path(r'social/', include('social_django.urls', namespace='social')),
     path('contents/add/', ContentCreate.as_view(), name='content-add'),
-    path('contents/<int:pk>/', ContentUpdate.as_view(), name='content-update'),
+    path('contents/<int:pk>/update', ContentUpdate.as_view(), name='content-update'),
     path('contents/<int:pk>/delete/', ContentDelete.as_view(), name='content-delete')
 ]
