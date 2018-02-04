f30480f2169873b87ad2a11baf291bc02561aeb6
John Claro <jkrclaro@gmail.com>
2018-02-04 15:26:19 +0000

    Starting templates

--- src/contents/views.py
+++ src/contents/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Content
+
+
+class ContentCreate(CreateView):
+    model = Content
+    fields = '__all__'
+
+
+class ContentUpdate(UpdateView):
+    model = Content
+    fields = '__all__'
+
+
+class ContentDelete(DeleteView):
+    model = Content
+    success_url = reverse_lazy('Content-list')
