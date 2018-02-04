ee895992910690d40c10e1af9f1c073151190d4c
John Claro <jkrclaro@outlook.com>
2018-02-04 22:06:09 +0000

    More improvements

--- src/contents/views.py
+++ src/contents/views.py
@@ -1,19 +1,31 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 
 from .models import Content
 
 
-class ContentCreate(CreateView):
+class ContentCreate(LoginRequiredMixin, CreateView):
     model = Content
     fields = '__all__'
 
 
-class ContentUpdate(UpdateView):
+class ContentUpdate(LoginRequiredMixin, UpdateView):
     model = Content
     fields = '__all__'
 
 
-class ContentDelete(DeleteView):
+class ContentDelete(LoginRequiredMixin, DeleteView):
     model = Content
     success_url = reverse_lazy('Content-list')
+
+
+class ContentDetailView(LoginRequiredMixin, DetailView):
+    model = Content
+
+
+class ContentListView(LoginRequiredMixin, ListView):
+    model = Content
+
--- src/urls.py
+++ src/urls.py
@@ -3,17 +3,21 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.views.generic import TemplateView
 
-from src.contents.views import ContentCreate, ContentDelete, ContentUpdate
+from src.contents import views as content_views
 
 
 admin.autodiscover()
 
 urlpatterns = [
-    re_path(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    re_path(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     re_path(r'^accounts/', include('allauth.urls')),
     path(r'admin/', admin.site.urls),
     path(r'social/', include('social_django.urls', namespace='social')),
-    path('contents/add/', ContentCreate.as_view(), name='content-add'),
-    path('contents/<int:pk>/update', ContentUpdate.as_view(), name='content-update'),
-    path('contents/<int:pk>/delete/', ContentDelete.as_view(), name='content-delete')
+
+    # Contents
+    path('contents/', content_views.ContentListView.as_view(), name='content-list'),
+    path('contents/add/', content_views.ContentCreate.as_view(), name='content-add'),
+    path('contents/<int:pk>/', content_views.ContentDetailView.as_view(), name='content-detail'),
+    path('contents/<int:pk>/update', content_views.ContentUpdate.as_view(), name='content-update'),
+    path('contents/<int:pk>/delete/', content_views.ContentDelete.as_view(), name='content-delete')
 ]
