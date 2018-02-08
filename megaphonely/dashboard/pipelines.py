7cbb66fdb32af152be3a9b1972004d4b61fa9253
John Claro <jkrclaro@outlook.com>
2018-02-08 11:08:42 +0000

    Add linkedin

--- megaphonely/dashboard/pipelines.py
+++ megaphonely/dashboard/pipelines.py
@@ -5,4 +5,7 @@ def upsert(user=None, response=None, backend=None, **kwargs):
     if not user:
         raise ValueError('You must login first')
 
+    print('YOU MADE ITTTTT YOU MADE ITTTTTYOU MADE ITTTTTYOU MADE ITTTTTYOU MADE ITTTTT')
+    print(backend.name)
+    print('YOU MADE ITTTTT YOU MADE ITTTTTYOU MADE ITTTTTYOU MADE ITTTTTYOU MADE ITTTTT')
     Social.objects.upsert(user, backend.name, response)
