ac2504a76f0019a6dfbcb1281630e7fe7d7f6080
John Claro <jkrclaro@outlook.com>
2018-02-08 11:19:56 +0000

    Add python3-linkedin

--- megaphonely/dashboard/pipelines.py
+++ megaphonely/dashboard/pipelines.py
@@ -5,7 +5,5 @@ def upsert(user=None, response=None, backend=None, **kwargs):
     if not user:
         raise ValueError('You must login first')
 
-    print('YOU MADE ITTTTT YOU MADE ITTTTTYOU MADE ITTTTTYOU MADE ITTTTTYOU MADE ITTTTT')
-    print(backend.name)
-    print('YOU MADE ITTTTT YOU MADE ITTTTTYOU MADE ITTTTTYOU MADE ITTTTTYOU MADE ITTTTT')
-    Social.objects.upsert(user, backend.name, response)
+    name = 'linkedin' if 'linkedin' in backend.name else backend.name
+    Social.objects.upsert(user, name, response)
