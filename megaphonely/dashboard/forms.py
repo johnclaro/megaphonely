8d8c2298193578dc1a29424b2be2c5ca1a421106
John Claro <jkrclaro@outlook.com>
2018-02-09 13:21:54 +0000

    More rispy forms

--- megaphonely/dashboard/forms.py
+++ megaphonely/dashboard/forms.py
@@ -1,25 +1,37 @@
 from django import forms
 
-from crispy_forms.layout import Submit
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Fieldset, ButtonHolder
+from crispy_forms.bootstrap import AccordionGroup, Accordion
 
 from .models import Content
+
+
+
 
 
 class ContentForm(forms.ModelForm):
 
     class Meta:
         model = Content
-        fields = ['message', 'schedule_at', 'multimedia']
+        fields = '__all__'
         exclude = ['account']
-        widgets = {'schedule_at': forms.SplitDateTimeWidget(
-            date_attrs={'type': 'date'},
-            time_attrs={'type': 'time'},
-            time_format='HH:mm'
-        )}
+        labels = {
+            'is_schedule_now': 'Schedule now',
+            'is_auto_schedule': 'Auto schedule'
+        }
 
     def __init__(self, *args, **kwargs):
         super(ContentForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset(
+                'Han Sung',
+                'message',
+                'multimedia',
+            ),
+            ButtonHolder(
+                Submit('submit', 'Submit', css_class='button white')
+            )
+        )
         self.helper.form_method = 'post'
-        self.helper.add_input(Submit('schedule', 'Schedule'))
