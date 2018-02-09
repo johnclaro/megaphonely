45c4abe9f5e1a34567ecb1522de96fb3d6d8327d
John Claro <jkrclaro@outlook.com>
2018-02-09 19:17:38 +0000

    Ability to update content

--- megaphonely/dashboard/models.py
+++ megaphonely/dashboard/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.urls import reverse
 from django.conf import settings
-from django.utils import timezone
 
 from .managers import ContentManager, SocialManager
 
