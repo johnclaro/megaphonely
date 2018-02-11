d71540e7884ad54df94b2287b2dcff7687e10f6d
John Claro <jkrclaro@outlook.com>
2018-02-11 16:56:24 +0000

    Disconnect social

--- megaphonely/dashboard/urls.py
+++ megaphonely/dashboard/urls.py
@@ -2,8 +2,8 @@ from django.urls import re_path
 
 from .views import (
     index,
-    ContentList, ContentDetail, ContentCreate, ContentUpdate, ContentDelete,
-    SocialList
+    ContentDetail, ContentCreate, ContentUpdate, ContentDelete,
+    disconnect, SocialDetail
 )
 
 app_name = 'dashboard'
@@ -14,6 +14,7 @@ urlpatterns = [
     re_path(r'^contents/(?P<pk>\d+)/$', ContentDetail.as_view(), name='content_detail'),
     re_path(r'^contents/(?P<pk>\d+)/edit/$', ContentUpdate.as_view(), name='content_update'),
     re_path(r'^contents/(?P<pk>\d+)/delete/$', ContentDelete.as_view(), name='content_delete'),
-    re_path(r'^socials/$', SocialList.as_view(), name='social_list'),
+    re_path(r'^socials/(?P<pk>\d+)/$', SocialDetail.as_view(), name='social_detail'),
+    re_path(r'^socials/(?P<pk>\d+)/disconnect/$', disconnect, name='social_disconnect'),
 ]
 
