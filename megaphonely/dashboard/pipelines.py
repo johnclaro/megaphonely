a5eac51dc0a6d06d2ae3fbfd7a5ee6a846fb73b6
John Claro <jkrclaro@outlook.com>
2018-02-14 14:07:06 +0000

    Manage FB Page & Group and LinkedIn Company

--- megaphonely/dashboard/pipelines.py
+++ megaphonely/dashboard/pipelines.py
@@ -5,6 +5,11 @@ def upsert(user=None, response=None, backend=None, **kwargs):
     if not user:
         raise ValueError('You must login first')
 
-    name = 'linkedin' if 'linkedin-oauth2' == backend.name else backend.name
+    name = backend.name
+    if 'linkedin' in name:
+        name = 'linkedin'
+    elif 'facebook' in name:
+        name = 'facebook'
+
     social = Social.objects.upsert(name, response)
     social.accounts.add(user)
