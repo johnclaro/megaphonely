92d6919d16ac6a9e913f44ed4e1fd70974752776
John Claro <jkrclaro@outlook.com>
2018-02-25 23:24:25 +0000

    Better layout

--- megaphonely/dashboard/views.py
+++ megaphonely/dashboard/views.py
@@ -114,3 +114,14 @@ class ContentList(LoginRequiredMixin, ListView):
         user = self.request.user
         contents = Content.objects.filter(account=user)
         return contents
+
+
+class SocialList(LoginRequiredMixin, ListView):
+    template_name = 'socials/list.html'
+    model = Social
+    context_object_name = 'socials'
+
+    def get_queryset(self):
+        user = self.request.user
+        socials = Social.objects.filter(accounts__in=[user])
+        return socials
