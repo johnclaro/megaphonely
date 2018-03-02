93b8430a7e00bc4cff555dc122a4141703528ac3
John Claro <jkrclaro@gmail.com>
2018-03-02 18:10:53 +0000

    Path and re_path

--- megaphonely/accounts/urls.py
+++ megaphonely/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from django.conf.urls import include
 from django.contrib.auth.views import LoginView
 
@@ -8,8 +8,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    re_path('^signup/$', views.signup, name='signup'),
-    re_path('^login/$', LoginView.as_view(authentication_form=LoginForm, redirect_authenticated_user=True), name='login'),
+    path('signup$', views.signup, name='signup'),
+    path('login/', LoginView.as_view(authentication_form=LoginForm, redirect_authenticated_user=True), name='login'),
     re_path('^', include('django.contrib.auth.urls')),
     re_path(r'^(?P<pk>\d+)/edit/$', views.ProfileUpdate.as_view(), name='profile_update'),
 ]
