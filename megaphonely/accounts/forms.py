49a3b4c006cd5236e5b700638760c5df4ac0d2b0
John Claro <jkrclaro@outlook.com>
2018-03-02 19:51:12 +0000

    TOS

--- megaphonely/accounts/forms.py
+++ megaphonely/accounts/forms.py
@@ -12,7 +12,7 @@ MyUser = get_user_model()
 
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class':'validate','placeholder': 'Username'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
 
 
--- megaphonely/accounts/urls.py
+++ megaphonely/accounts/urls.py
@@ -8,7 +8,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup$', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
     path('login/', LoginView.as_view(authentication_form=LoginForm, redirect_authenticated_user=True), name='login'),
     re_path('^', include('django.contrib.auth.urls')),
     re_path(r'^(?P<pk>\d+)/edit/$', views.ProfileUpdate.as_view(), name='profile_update'),
