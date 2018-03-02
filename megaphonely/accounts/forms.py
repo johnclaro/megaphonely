7da5983af38e10c9908d39aa07e031166b739905
John Claro <jkrclaro@outlook.com>
2018-03-02 22:30:43 +0000

    Password reset

--- megaphonely/accounts/forms.py
+++ megaphonely/accounts/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 
 from crispy_forms.helper import FormHelper
@@ -12,17 +11,7 @@ from .models import Profile
 User = get_user_model()
 
 
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
-
-
 class SignupForm(UserCreationForm):
-    username = forms.CharField(label='username', widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    email = forms.EmailField(label='email', widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    password1 = forms.CharField(label='password1', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-    password2 = forms.CharField(label='password2', widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}))
-
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
--- megaphonely/accounts/urls.py
+++ megaphonely/accounts/urls.py
@@ -2,14 +2,12 @@ from django.urls import re_path, path
 from django.conf.urls import include
 from django.contrib.auth.views import LoginView
 
-from .forms import LoginForm
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', LoginView.as_view(authentication_form=LoginForm, redirect_authenticated_user=True), name='login'),
-    re_path('^', include('django.contrib.auth.urls')),
+    path('login/', LoginView.as_view(redirect_authenticated_user=True), name='login'),
     re_path(r'^(?P<pk>\d+)/edit/$', views.ProfileUpdate.as_view(), name='profile_update'),
 ]
