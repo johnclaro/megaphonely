c1c0cc36b5ab048f257576af49030250cdc55e0c
John Claro <jkrclaro@gmail.com>
2018-03-03 16:12:37 +0000

    Better

--- megaphonely/dashboard/urls.py
+++ megaphonely/dashboard/urls.py
@@ -1,21 +1,26 @@
-from django.urls import re_path
+from django.urls import re_path, path
+from django.views.generic import TemplateView
 
 from .views import (
     index,
-    ContentList, ContentDetail, ContentCreate, ContentUpdate, ContentDelete,
-    social_disconnect, SocialList
+    ContentList,
+    ContentDetail,
+    ContentCreate,
+    ContentUpdate,
+    ContentDelete,
+    social_disconnect,
 )
 
 app_name = 'dashboard'
 
 urlpatterns = [
-    re_path(r'^$', index, name='index'),
-    re_path(r'^contents/$', ContentList.as_view(), name='content_list'),
-    re_path(r'^contents/create/$', ContentCreate.as_view(), name='content_add'),
+    path('', index, name='index'),
+    path('connect/', TemplateView.as_view(template_name='socials/connect.html'), name='connect'),
+    path('contents/', ContentList.as_view(), name='content_list'),
+    path('contents/create/', ContentCreate.as_view(), name='content_add'),
     re_path(r'^contents/(?P<pk>\d+)/$', ContentDetail.as_view(), name='content_detail'),
     re_path(r'^contents/(?P<pk>\d+)/edit/$', ContentUpdate.as_view(), name='content_update'),
     re_path(r'^contents/(?P<pk>\d+)/delete/$', ContentDelete.as_view(), name='content_delete'),
-    re_path(r'^socials/$', SocialList.as_view(), name='social_list'),
     re_path(r'^socials/(?P<pk>\d+)/disconnect/$', social_disconnect, name='social_disconnect'),
 ]
 
--- megaphonely/urls.py
+++ megaphonely/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls import include
-from django.urls import re_path, path
+from django.urls import path
 from django.views.generic import TemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -10,25 +10,25 @@ admin.autodiscover()
 
 
 urlpatterns = [
-    re_path(r'^', include('megaphonely.dashboard.urls', namespace='dashboard')),
-    re_path(r'^', include('allauth.urls')),
+    path('', include('megaphonely.dashboard.urls', namespace='dashboard')),
+    path('', include('allauth.urls')),
+    path('profiles/', include('megaphonely.accounts.urls', namespace='accounts')),
     path('contact-us/', TemplateView.as_view(template_name='support/contact_us.html'), name='contact_us'),
     path('privacy/', TemplateView.as_view(template_name='legal/privacy.html'), name='privacy'),
     path('terms/', TemplateView.as_view(template_name='legal/terms.html'), name='terms'),
     path('billing/', TemplateView.as_view(template_name='billing/index.html'), name='billing'),
     path('pricing/', TemplateView.as_view(template_name='billing/pricing.html'), name='pricing'),
-    re_path(r'^profiles/', include('megaphonely.accounts.urls', namespace='accounts')),
-    re_path(r'^social/', include('social_django.urls', namespace='social')),
+    path('social/', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
-        re_path(r'^admin/', admin.site.urls),
-        re_path(r'^__debug__/', include(debug_toolbar.urls)),
+        path('admin/', admin.site.urls),
+        path('__debug__/', include(debug_toolbar.urls)),
     ]
     urlpatterns += staticfiles_urlpatterns()
 else:
     urlpatterns += [
-        re_path(r'^__6TJny9S332qv92p57585kZdM9srNA66N2s26M39U4M2232B8Uz/', admin.site.urls),
+        path('__6TJny9S332qv92p57585kZdM9srNA66N2s26M39U4M2232B8Uz/', admin.site.urls),
     ]
