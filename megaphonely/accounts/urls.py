951bcf920d9c39c797bc39d86c299037a05d8b26
John Claro <jkrclaro@gmail.com>
2018-03-27 17:09:34 +0100

    Hide PK when editing profile

--- megaphonely/accounts/urls.py
+++ megaphonely/accounts/urls.py
@@ -1,9 +1,9 @@
-from django.urls import re_path
+from django.urls import path
 
 from .views import ProfileUpdate
 
 app_name = 'accounts'
 
 urlpatterns = [
-    re_path(r'^(?P<pk>\d+)/edit/$', ProfileUpdate.as_view(), name='profile_update'),
+    path('edit/', ProfileUpdate.as_view(), name='profile_update'),
 ]
