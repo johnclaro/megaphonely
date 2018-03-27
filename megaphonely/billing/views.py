e2d41c437de3eb9016117e67006e6f5fae176958
John Claro <jkrclaro@outlook.com>
2018-03-27 18:01:53 +0100

    Fix bug when content is initially scheduled but changed to now

--- megaphonely/billing/views.py
+++ megaphonely/billing/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import redirect
 from django.conf import settings
 from django.template import loader
 from django.http import HttpResponse
-from django.http import Http404
 
 import stripe
 
@@ -46,7 +45,7 @@ def subscribe(request):
     plan_id = settings.STRIPE_PLANS[plan]['id']
 
     customer = stripe.Customer.create(email=user.email)
-    Customer.objects.create( 
+    Customer.objects.create(
         account=user, customer_id=customer['id'], plan='trial'
     )
 
