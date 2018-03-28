a0281e14d8ff8016626c394d55f5ff7f8f630fae
John Claro <jkrclaro@outlook.com>
2018-03-28 18:09:22 +0100

    Trial expiration flash messages

--- megaphonely/dashboard/pipelines.py
+++ megaphonely/dashboard/pipelines.py
@@ -1,6 +1,8 @@
 from .models import Social
 
 from django.contrib import messages
+from django.utils import timezone
+from django.utils.safestring import mark_safe
 
 
 def upsert(user=None, response=None, backend=None, request=None, **kwargs):
@@ -12,6 +14,16 @@ def upsert(user=None, response=None, backend=None, request=None, **kwargs):
     else:
         provider = backend.name
 
-    capped, level, message = Social.objects.upsert(provider, response, user)
+    if user.trial.ends_at < timezone.now():
+        level = messages.ERROR
+        message = mark_safe("""Your trial has expired but you can still
+        <a href='mailto:support@megaphonely.com?subject=Extend%20trial'>contact us</a>
+        if you would still like to extend. We also appreciate feedback
+        if you could include it in your email!
+        """)
+        capped = True
+    else:
+        capped, level, message = Social.objects.upsert(provider, response, user)
+
     if capped:
         messages.add_message(request, level, message)
