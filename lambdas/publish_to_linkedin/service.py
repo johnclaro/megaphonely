d15d5176952494bc9629594cb05fbdb8ed662c31
John Claro <jkrclaro@gmail.com>
2018-03-31 13:07:58 +0100

    Update linkedin lambda

--- lambdas/publish_to_linkedin/service.py
+++ lambdas/publish_to_linkedin/service.py
@@ -1,30 +1,41 @@
 # -*- coding: utf-8 -*-
 import os
+from typing import BinaryIO
 
 import boto3
 from linkedin import linkedin
 
 
-def download_s3_file(s3_key: str, s3_bucket_name: str) -> None:
-    s3_object = boto3.resource('s3').Object(s3_bucket_name, s3_key)
-    s3_object.download_file(s3_key)
+def download_s3_file(s3_key: str, s3_bucket_name: str) -> str:
+    s3 = boto3.resource('s3')
+    filename = os.path.basename(s3_key)
+    tmp_filename = f'/tmp/{filename}'
+    s3.Bucket(s3_bucket_name).download_file(s3_key, tmp_filename)
+    return tmp_filename
 
 
-def get_s3_multimedia_content(payload: dict, key: str, s3_key: str,
-                              s3_bucket_name: str) -> dict:
-    download_s3_file(s3_key, s3_bucket_name)
-    payload[key] = open(s3_key, 'rb')
-    os.remove(s3_key)
-    return payload
+def get_s3_multimedia_content(s3_key: str, s3_bucket_name: str) -> BinaryIO:
+    tmp_filename = download_s3_file(s3_key, s3_bucket_name)
+    source = open(tmp_filename, 'rb')
+    os.remove(tmp_filename)
+    return source
 
 def handler(event, context):
     s3_bucket_name = event['s3_bucket_name']
     access_token_key = event['access_token_key']
     message = event['message']
-    video = ''
-    image = ''
+    image = event['image']
+
+    data = {
+        'title': message, 'visibility_code': 'connections-only',
+        'comment': message
+    }
+    if image:
+        url = f'https://s3-eu-west-1.amazonaws.com/{s3_bucket_name}/{image}'
+        data['submitted_url'] = url
+        data['submitted_image_url'] = url
 
     application = linkedin.LinkedInApplication(token=access_token_key)
-    response = application.submit_share(comment=message,
-                                        visibility_code='connections-only')
+    response = application.submit_share(**data)
+
     return response
