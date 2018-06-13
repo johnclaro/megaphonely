bd82a94480c1d4eb2fb4123f1f695b68b6a14a79
John Claro <jkrclaro@gmail.com>
2018-06-14 00:38:35 +0100

    Stuff

--- megaphonely/app.py
+++ megaphonely/app.py
@@ -2,9 +2,16 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore
+from flask_migrate import Migrate
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+from .forms import ExtendedRegisterForm
 
 db = SQLAlchemy()
-security = Security()
+security = Security(register_form=ExtendedRegisterForm)
+migrate = Migrate()
+admin = Admin(name='Megaphonely', template_mode='bootstrap3')
 
 def create_app():
     app = Flask(__name__)
@@ -12,10 +19,14 @@ def create_app():
     app.config.from_object(config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from .models import User, Role
     users = SQLAlchemyUserDatastore(db, User, Role)
     security.init_app(app, users)
+
+    admin.init_app(app)
+    admin.add_view(ModelView(User, db.session))
 
     from .views.home import home
     from .views.account import account
--- megaphonely/forms.py
+++ megaphonely/forms.py
@@ -1,6 +1,6 @@
-from flask_wtf import FlaskForm
+from flask_security.forms import RegisterForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+class ExtendedRegisterForm(RegisterForm):
+    username = StringField('Username', [DataRequired()])
