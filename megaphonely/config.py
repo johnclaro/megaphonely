c1a1a2f85b080d5c03d3b3c07ce08e6b465e08dc
John Claro <jkrclaro@gmail.com>
2018-06-13 23:50:03 +0100

    Login and register

--- megaphonely/config.py
+++ megaphonely/config.py
@@ -3,6 +3,7 @@ import os
 class Config(object):
     SECRET_KEY = os.environ['SECRET_KEY']
     SECURITY_REGISTERABLE = True
+    SECURITY_RECOVERABLE = True
 
 class Production(Config):
     SECRET_KEY = 'production'
