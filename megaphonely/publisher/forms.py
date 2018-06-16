d6400d7039f7efbacf5c23f7978c09de874bc310
John Claro <jkrclaro@gmail.com>
2018-06-16 15:54:21 +0100

    Image issue

--- megaphonely/publisher/forms.py
+++ megaphonely/publisher/forms.py
@@ -17,7 +17,6 @@ class ContentForm(forms.ModelForm):
                        'placeholder': 'What do you want to tell your audience?',
                        'rows': 4, 'cols': 15}
             ),
-            'multimedia': forms.FileInput(attrs={'class': 'col-sm-12'}),
             'socials': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
             'schedule': forms.Select(attrs={'class': 'form-control'}),
             'schedule_at': forms.TextInput(
