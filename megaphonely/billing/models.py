484223e1967afd1e2258d8b27111a237a78b5173
John Claro <jkrclaro@outlook.com>
2018-06-19 22:25:54 +0100

    More billing switcheroos

--- megaphonely/billing/models.py
+++ megaphonely/billing/models.py
@@ -6,8 +6,7 @@ from django.conf import settings
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
-from .managers import (CustomerManager, SubscriptionManager,
-                       PaymentMethodManager)
+from .managers import CustomerManager, SubscriptionManager, PaymentMethodManager
 from .choices import PLANS
 
 
@@ -16,9 +15,7 @@ def get_trial_ends_at():
 
 
 class Customer(models.Model):
-    stripe_customer_id = models.CharField(max_length=50, blank=True)
-    plan = models.CharField(max_length=20, choices=PLANS, default='free')
-    ends_at = models.DateTimeField(default=get_trial_ends_at)
+    stripe_customer_id = models.CharField(max_length=100, primary_key=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -29,10 +26,7 @@ class Customer(models.Model):
     objects = CustomerManager()
 
     def __str__(self):
-        return self.account.username
-
-    def get_ends_at_date(self):
-        return self.ends_at.date()
+        return self.stripe_customer_id
 
     @receiver(post_save, sender=settings.AUTH_USER_MODEL)
     def create_customer(sender, instance, created, **kwargs):
@@ -45,7 +39,7 @@ class Customer(models.Model):
 
 
 class PaymentMethod(models.Model):
-    stripe_source_id = models.CharField(max_length=100)
+    stripe_source_id = models.CharField(max_length=100, primary_key=True)
     stripe_last_four = models.CharField(max_length=4)
     stripe_brand = models.CharField(max_length=20)
 
@@ -61,14 +55,17 @@ class PaymentMethod(models.Model):
 
 
 class Subscription(models.Model):
-    stripe_subscription_id = models.CharField(max_length=50)
+    stripe_subscription_id = models.CharField(max_length=100, primary_key=True)
+    plan = models.CharField(max_length=20, choices=PLANS, default='free')
+    ends_at = models.DateTimeField(default=get_trial_ends_at)
+    is_active = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     objects = SubscriptionManager()
 
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
     payment_method = models.ForeignKey(PaymentMethod, on_delete=models.CASCADE)
 
     def __str__(self):
--- megaphonely/publisher/managers.py
+++ megaphonely/publisher/managers.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
 from django.contrib import messages
-from django.utils import timezone
 
 from facepy import GraphAPI
 from linkedin import linkedin
