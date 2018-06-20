a12b83b8bd79ee1996e24489808215e6fca90a96
John Claro <jkrclaro@outlook.com>
2018-06-20 10:50:21 +0100

    Use social pictures

--- megaphonely/publisher/forms.py
+++ megaphonely/publisher/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, HTML, ButtonHolder, Submit
-from crispy_forms.bootstrap import InlineRadios
+from crispy_forms.layout import Layout, ButtonHolder, Submit
 
 from .models import Content, Social, Team
 
@@ -36,54 +35,6 @@ class ContentForm(forms.ModelForm):
         super(ContentForm, self).__init__(*args, **kwargs)
         socials = Social.objects.filter(account=account)
         self.fields['socials'].queryset = socials
-
-
-class OldContentForm(forms.ModelForm):
-    class Meta:
-        model = Content
-        fields = ['message', 'multimedia', 'schedule', 'schedule_at', 'socials']
-        widgets = {
-            'socials': forms.RadioSelect(
-                attrs={
-                    'class': 'socials-multiple',
-                    'multiple': 'multiple',
-                    'name': 'socials[]'
-                }
-            ),
-            'schedule_at': forms.TextInput(
-                attrs={
-                    'class': 'datetimepicker-input',
-                    'data-target': '#id_schedule_at',
-                    'data-toggle': 'datetimepicker'
-                }
-            )
-        }
-        labels = {'multimedia': 'Image'}
-
-    def __init__(self, *args, **kwargs):
-        account = kwargs.pop('account')
-        super(ContentForm, self).__init__(*args, **kwargs)
-        socials = Social.objects.filter(account=account)
-        self.fields['socials'].queryset = socials
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            'message',
-            'multimedia',
-            'socials',
-            InlineRadios('schedule'),
-            'schedule_at',
-            HTML(
-                """
-                <button type='submit' class='btn btn-primary btn-block'>
-                    <i id="twitter_content" class="fab fa-twitter-square" style="color: white; display: none;"></i>
-                    <i id="facebook_content" class="fab fa-facebook" style="color: white; display: none;"></i>
-                    <i id="linkedin_content" class="fab fa-linkedin" style="color: white; display: none;"></i>
-                   Share
-                </button>
-                """
-            )
-        )
-        self.helper.form_method = 'post'
 
 
 class TeamForm(forms.ModelForm):
