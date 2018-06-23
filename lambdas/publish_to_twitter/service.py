bad4cfdcd58a7658efd295dec508601a5ef45339
John Claro <jkrclaro@gmail.com>
2018-06-23 22:45:49 +0100

    Upload locally

--- lambdas/publish_to_twitter/service.py
+++ lambdas/publish_to_twitter/service.py
@@ -14,8 +14,11 @@ def download_s3_file(s3_key: str, s3_bucket_name: str) -> str:
     return tmp_filename
 
 
-def get_s3_multimedia_content(s3_key: str, s3_bucket_name: str) -> BinaryIO:
-    tmp_filename = download_s3_file(s3_key, s3_bucket_name)
+def get_s3_multimedia_content(s3_key: str, s3_bucket_name: str,
+                              tmp_filename: str) -> BinaryIO:
+    print("Got temp file:", tmp_filename)
+    if not tmp_filename:
+        tmp_filename = download_s3_file(s3_key, s3_bucket_name)
     source = open(tmp_filename, 'rb')
     os.remove(tmp_filename)
     return source
@@ -23,6 +26,7 @@ def get_s3_multimedia_content(s3_key: str, s3_bucket_name: str) -> BinaryIO:
 
 def handler(event, context):
     # https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-statuses-update
+    tmp_filename = event['tmp_filename']
     consumer_key = event['consumer_key']
     consumer_secret = event['consumer_secret']
     s3_bucket_name = event['s3_bucket_name']
@@ -39,7 +43,8 @@ def handler(event, context):
     )
     data = {}
     if image:
-        data['media'] = get_s3_multimedia_content(image, s3_bucket_name)
+        data['media'] = get_s3_multimedia_content(image, s3_bucket_name,
+                                                  tmp_filename)
     response = api.PostUpdate(message, **data)
     response_json = response.AsDict()
     return response_json
