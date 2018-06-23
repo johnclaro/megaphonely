bdbe1a0cc8c49520385721f5ddd7602423765606
John Claro <jkrclaro@outlook.com>
2018-06-23 23:28:19 +0100

    Upload videos to linkedin

--- lambdas/publish_to_linkedin/service.py
+++ lambdas/publish_to_linkedin/service.py
@@ -23,38 +23,37 @@ def get_s3_multimedia_content(s3_key: str, s3_bucket_name: str) -> BinaryIO:
 
 def handler(event, context):
     # https://developer.linkedin.com/docs/share-on-linkedin
+    tmp_filename = event.get('tmp_filename', '')
     access_token_key = event['access_token_key']
     message = event['message']
-    image = event['image']
+    multimedia = event['multimedia']
     cloudfront = event['cloudfront']
-    company_id = event['company_id']
+    company_id = event.get('company_id', '')
+    is_image = event['is_image']
 
     data = {
-        'title': message, 'visibility_code': 'connections-only',
+        'title': message,
+        'visibility_code': 'anyone' if tmp_filename else 'anyone',
         'comment': message
     }
-    if image:
-        url = f'https://{cloudfront}/{image}'
-        data['submitted_url'] = url
-        data['submitted_image_url'] = url
 
-    data = {
-        'title': message, 'visibility_code': 'connections-only',
-        'comment': message
-    }
-    if image:
-        url = f'https://{cloudfront}/{image}'
+    if multimedia:
+        if tmp_filename:
+            if is_image:
+                url = 'https://s3-eu-west-1.amazonaws.com/megaphonely.com/media/contents/leagueoflegends-1528732699028-4851.jpg'
+            else:
+                url = 'https://s3-eu-west-1.amazonaws.com/megaphonely.com/media/contents/bus.mp4'
+        else:
+            url = f'https://{cloudfront}/{multimedia}'
         data['submitted_url'] = url
         data['submitted_image_url'] = url
 
     application = linkedin.LinkedInApplication(token=access_token_key)
     if company_id:
         data['company_id'] = company_id
-        data['visibility_code'] = 'anyone'
+        print("Posting company share...")
         response = application.submit_company_share(**data)
     else:
         response = application.submit_share(**data)
-
-
 
     return response
