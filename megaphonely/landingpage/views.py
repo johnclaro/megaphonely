11200abb7fc1c275bd18bb2af112b272879d617b
John Claro <jkrclaro@outlook.com>
2018-06-27 12:58:41 +0100

    Footer

--- megaphonely/landingpage/views.py
+++ megaphonely/landingpage/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def signup(request):
+    context = {}
+    template = 'landingpage/index.html'
+    response = render(request, template, context)
+
+    return response
