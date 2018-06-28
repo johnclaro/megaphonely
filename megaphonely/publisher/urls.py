cf1604c2856308069eebdf2eb88ddef876767b6e
John Claro <jkrclaro@outlook.com>
2018-06-28 18:02:30 +0100

    Remove /

--- megaphonely/publisher/urls.py
+++ megaphonely/publisher/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('connect/', TemplateView.as_view(template_name='socials/connect.html'), name='connect'),
     path('prompt/', social_prompt, name='prompt'),
 
-    path('dashboard/', ContentCreate.as_view(), name='content_create'),
+    path('dashboard', ContentCreate.as_view(), name='content_create'),
     re_path(r'^contents/(?P<pk>\d+)/update/$', ContentUpdate.as_view(), name='content_update'),
     re_path(r'^contents/(?P<pk>\d+)/delete/$', ContentDelete.as_view(), name='content_delete'),
 ]
