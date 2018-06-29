a9ea06715a994b240c31b71efabc075aa6a1cc09
John Claro <jkrclaro@outlook.com>
2018-06-29 18:52:09 +0100

    Checkbox js

--- static/megaphonely/js/checkboxes.js
+++ static/megaphonely/js/checkboxes.js
@@ -1,14 +1,22 @@
-$(function() {
+function checkboxValidation() {
     const checkboxes = $('#checkboxes');
     const composer = $('#composer');
     const composerAlert = $('#composer-alert');
+    const twitterCheckboxAlert = $('#twitter-checkbox-alert');
 
     let anyChecked;
+    let twitterChecked;
+    let twitterCheckboxId;
     $('#checkboxes input[type="checkbox"]').each(function() {
-        if ($(this).is(":checked")) {
+        if (this.checked) {
             anyChecked = true;
+
+            if (this.id.indexOf('twitter') >= 0) {
+                twitterChecked = true;
+                twitterCheckboxId = this.id;
+            }
         }
-        anyChecked ? composer.show() : composer.hide();
+
         if (anyChecked) {
             composer.show();
             composerAlert.hide();
@@ -18,19 +26,33 @@ $(function() {
         }
     })
 
-    $('#checkboxes input[type="checkbox"]').change(function() {
-        anyChecked = false;
+    if (twitterChecked) {
         $('#checkboxes input[type="checkbox"]').each(function() {
-            if (this.checked) {
-                anyChecked = true;
-            }
-            if (anyChecked) {
-                composer.show();
-                composerAlert.hide();
-            } else {
-                composer.hide();
-                composerAlert.show();
+            if (this.id.indexOf('twitter') >= 0 && this.id != twitterCheckboxId) {
+                let checkboxId = this.id.split('id_socials_')[1]
+                twitterCheckbox = $(`#${checkboxId}`);
+                twitterCheckbox.hide();
             }
         })
+    } else {
+        $('#checkboxes input[type="checkbox"]').each(function() {
+            if (this.id.indexOf('twitter')) {
+                let checkboxId = this.id.split('id_socials_')[1]
+                twitterCheckbox = $(`#${checkboxId}`);
+                twitterCheckbox.show();
+            }
+        })
+    }
+}
+
+$(function() {
+    // Initialise checkboxes
+    $('#checkboxes input[type="checkbox"]').ready(function() {
+        checkboxValidation();
+    })
+
+    // Trigger twitter checkboxes when it changes
+    $('#checkboxes input[type="checkbox"]').change(function() {
+        checkboxValidation();
     })
 });
