faf0573d5ab0ab4091e5444c402851a8e77817f1
John Claro <jkrclaro@gmail.com>
2018-08-06 17:52:35 +0100

    Working subscribe

--- megaphonely/landingpage/views.py
+++ megaphonely/landingpage/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
+from django.contrib import messages
 
+from .models import Charlie
 from .forms import CharlieForm
 
 
 def index(request):
-    form = CharlieForm()
+    if request.method == 'POST':
+        form = CharlieForm(request.POST)
+        if form.is_valid():
+            payload = request.POST
+            charlie = Charlie(
+                email=payload['email'],
+                kind=payload['kind'],
+                category=payload['category']
+            )
+            charlie.save()
+            messages.success(request, 'Thanks for signing up!')
+    else:
+        form = CharlieForm()
     context = {'form': form}
     return render(request, 'landingpage/index.html', context)
 
 
 def signup(request):
+    if request.method == 'POST':
+        payload = request.POST
+        charlie = Charlie(
+            email=payload['email'],
+            kind=payload['kind'],
+            category=payload['category']
+        )
+        charlie.save()
     return render(request, 'landingpage/index.html')
