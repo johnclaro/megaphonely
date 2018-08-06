b5a8457573cedf9931c998f75030a50e447c7674
John Claro <jkrclaro@outlook.com>
2018-08-06 18:41:06 +0100

    Must be unique email

--- megaphonely/landingpage/models.py
+++ megaphonely/landingpage/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 from .choices import KINDS, CATEGORIES
+from .managers import CharlieManager
 
 
 class Charlie(models.Model):
     email = models.EmailField(unique=True)
     kind = models.CharField(max_length=10, choices=KINDS)
     category = models.CharField(max_length=100, choices=CATEGORIES)
+    objects = CharlieManager()
 
     def __str__(self):
         return self.email
