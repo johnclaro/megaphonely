5edf18b7446e8056882954b65dae7591ffc4a2c8
John Claro <jkrclaro@gmail.com>
2018-08-06 21:03:15 +0100

    Email

--- megaphonely/landingpage/urls.py
+++ megaphonely/landingpage/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
-from .views import index, signup
+from .views import index
 
 app_name = 'landingpage'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('signup/', signup, name='signup')
 ]
--- megaphonely/landingpage/views.py
+++ megaphonely/landingpage/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .models import Charlie
 from .forms import CharlieForm
@@ -16,20 +18,26 @@ def index(request):
                 category=payload['category']
             )
             charlie.save()
-            messages.success(request, 'Thanks for signing up!')
+
+            msg = f"Thanks for signing up {payload['email']}!"
+            subject = 'Megaphonely'
+            html_message = """
+                <img src='https://assets.megaphonely.com/static/email/megaphonely.png'>
+                <h1>Thanks for joining the Megaphonely mailing list!</h1>
+                <p>
+                    You'll be updated on the latest developments of Megaphonely. Also, you can reply to this email if you want to suggest a feature or an idea that you'd like implemented.
+                </p>
+            """
+
+            send_mail(
+                subject=subject,
+                message=html_message,
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[payload['email']],
+                html_message=html_message
+            )
+            messages.success(request, msg)
     else:
         form = CharlieForm()
     context = {'form': form}
     return render(request, 'landingpage/index.html', context)
-
-
-def signup(request):
-    if request.method == 'POST':
-        payload = request.POST
-        charlie = Charlie(
-            email=payload['email'],
-            kind=payload['kind'],
-            category=payload['category']
-        )
-        charlie.save()
-    return render(request, 'landingpage/index.html')
