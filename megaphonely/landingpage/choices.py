fe778972990ec03bfdee2127d369b1cde612fb89
John Claro <jkrclaro@outlook.com>
2018-08-06 17:37:27 +0100

    Kind and categories

---
+++ megaphonely/landingpage/choices.py
@@ -0,0 +1,30 @@
+KINDS = (
+    ('brand', 'Brand'),
+    ('influencer', 'Influencer'),
+)
+
+CATEGORIES = (
+    ('books', 'Books'),
+    ('business', 'Business'),
+    ('education', 'Education'),
+    ('entertainment', 'Entertainment'),
+    ('finance', 'Finance'),
+    ('food & drink', 'Food & Drink'),
+    ('games', 'Games'),
+    ('health & fitness', 'Health & Fitness'),
+    ('kids', 'Kids'),
+    ('lifestyle', 'Lifestyle'),
+    ('medical', 'Medical'),
+    ('music', 'Music'),
+    ('navigation', 'Navigation'),
+    ('news', 'News'),
+    ('photo & video', 'Photo & Video'),
+    ('productivity', 'Productivity'),
+    ('reference', 'Reference'),
+    ('social networking', 'Social Networking'),
+    ('shopping', 'Shopping'),
+    ('sports', 'Sports'),
+    ('travel', 'Travel'),
+    ('utilities', 'Utilities'),
+    ('weather', 'Weather'),
+)
--- megaphonely/landingpage/models.py
+++ megaphonely/landingpage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from .choices import KINDS, CATEGORIES
+
+
+class Charlie(models.Model):
+    email = models.EmailField(unique=True)
+    kind = models.CharField(max_length=10, choices=KINDS)
+    category = models.CharField(max_length=100, choices=CATEGORIES)
+
+    def __str__(self):
+        return self.email
