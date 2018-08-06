8f1dd7e168e6c62bc88c15b5718b5d1467d5afd5
John Claro <jkrclaro@outlook.com>
2018-08-06 17:52:35 +0100

    Working subscribe

--- megaphonely/landingpage/forms.py
+++ megaphonely/landingpage/forms.py
@@ -6,4 +6,4 @@ from .choices import KINDS, CATEGORIES
 class CharlieForm(forms.Form):
     email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email address'}))
     kind = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), choices=KINDS)
-    categories = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), choices=CATEGORIES)
+    category = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), choices=CATEGORIES)
